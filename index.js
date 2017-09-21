57ac484fd8ba3c934f3cac12f59e8ba4db66d044
Austin Huang <austinhuang0131@gmail.com>
2017-09-21 14:52:26 -0400

    Update index.js

--- index.js
+++ index.js
@@ -295,9 +295,9 @@ bot.on("message", message => {
 			message.channel.sendEmbed(new Discord.RichEmbed().setColor("#007FFF").setTitle("List of Commands").setDescription("For more information, use `>info`.").addField(">dial / >call", "Dial a number using your own number").addField(">pdial / >pcall", "Dial a number using public payphone, 8 credits per message").addField(">rdial / >rcall","Dial a random number in the phonebook (*411)").addField(">wizard","Get yourself a number").addField(">order","Get some virtual food").addField(">mailbox", "Check your number's mailbox, plus various settings").addField(">suggest", "Suggest something to be added to the bot").addField(">convert", "Convert your credits into other bot currency via [Discoin](http://discoin.sidetrip.xyz)").addField("Other commands about money", "`>daily`, `>lottery`").addField("You probably know how these commands work", "`>invite`, `>info`"));
 		}
 		else if(message.content.startsWith(">suggest")) {
-			if (msg.content.split(" ")[1] === undefined){
+			if (message.content.split(" ")[1] === undefined){
 				message.reply(":x: Usage: `>suggest <suggestion>`")
-				return
+				return;
 			}
 			message.reply("Thanks for your suggestion!");
 	    		bot.channels.get("326798754348793857").send("New suggestion from __" + message.author.username + "#" + message.author.discriminator + "__ (" + message.author.id + ") ```\n" + message.content.split(" ").splice(1).join(" ").split("```").join(" ") + "```");
