fee39fc7a370fae8f0cc54252f3659f724d549ea
Austin Huang <austinhuang0131@gmail.com>
2017-12-13 11:16:13 -0500

    Create suggest.js

---
+++ commands/suggest.js
@@ -0,0 +1,8 @@
+exports.run = (bot, message, args) => {
+  if (args === undefined){
+    message.reply(":x: Usage: `>suggest <suggestion>`")
+    return;
+  }
+  message.reply("Thanks for your suggestion!");
+  bot.channels.get("326798754348793857").send("New suggestion from __" + message.author.username + "#" + message.author.discriminator + "__ (" + message.author.id + ") ```\n" + message.content.split(" ").splice(1).join(" ").split("```").join(" ") + "```");
+}
