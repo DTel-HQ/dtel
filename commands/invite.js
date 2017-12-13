2415539ebe9f03b1e1c8485da41efd7d154da0fb
Austin Huang <austinhuang0131@gmail.com>
2017-12-13 11:14:17 -0500

    Create invite.js

---
+++ commands/invite.js
@@ -0,0 +1,3 @@
+exports.run = (bot, message, args) => {
+  message.reply("https://discordapp.com/oauth2/authorize?client_id=377609965554237453&scope=bot&permissions=67169284\n**All permissions are essential.**");
+};
