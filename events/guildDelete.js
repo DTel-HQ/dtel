0a0aa13e46bb58063da9173205be438747edff7e
Austin Huang <austinhuang0131@gmail.com>
2017-12-14 12:29:41 -0500

    Create guildDelete.js

---
+++ events/guildDelete.js
@@ -0,0 +1,29 @@
+exports.run = (guild) => {
+	var cleanedguildname = guild.name.replace(/discord\.(gg|io|me|li)\/([0-9]|[a-z])*/g, "**Invite link censored**");
+	bot.channels.get("282253502779228160").send(":outbox_tray: Left `"+guild.name+"` ("+guild.id+"). Currently in "+bot.guilds.array().length+" servers.");
+	bot.user.setPresence({game:{name:bot.guilds.array().length+" servers | >help", type: 0}});
+		request.post({
+		url: "https://bots.discord.pw/api/bots/377609965554237453/stats",
+		headers: {
+			"content-type": "application/json",
+			"Authorization": process.env.BOTS_PW_TOKEN
+		},
+		json: {
+			"server_count": bot.guilds.size.toString()
+		}
+	}, function(error, response, body) {
+		console.log("DBots returns success: "+ body);
+	});
+	request.post({
+		url: "https://discordbots.org/api/bots/377609965554237453/stats",
+		headers: {
+			"content-type": "application/json",
+			"Authorization": process.env.DBL_ORG_TOKEN
+		},
+		json: {
+			"server_count": bot.guilds.size.toString()
+		}
+	}, function(error, response, body) {
+		console.log("DBotsList returns success: "+ body);
+	});
+};
