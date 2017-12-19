c1c8247f84f6dc026d7d47fa17e27369275605e2
Austin Huang <austinhuang0131@gmail.com>
2017-12-19 16:29:38 -0500

    Update deassign.js

--- commands/deassign.js
+++ commands/deassign.js
@@ -1,4 +1,5 @@
-var numbers = JSON.parse(fs.readFileSync("././numbers.json", "utf8")),
+var fs = require("fs"),
+    numbers = JSON.parse(fs.readFileSync("././numbers.json", "utf8")),
     support = user_id => bot.guilds.get('281815661317980160').roles.get('281815839936741377').members.map(member => member.id).indexOf(user_id) > -1;
 
 exports.run = (bot, message, args) => {
