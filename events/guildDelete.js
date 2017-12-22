4fb1688451b30c02d3803c7e2720a1d3a1a17cac
Austin Huang <austinhuang0131@gmail.com>
2017-12-22 15:26:18 -0500

    refer program

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -1,3 +1,5 @@
+var referreds = JSON.parse(fs.readFileSync("././json/guilds.json", "utf8"));
+
 exports.run = (guild) => {
 	var cleanedguildname = guild.name.replace(/discord\.(gg|io|me|li)\/([0-9]|[a-z])*/g, "**Invite link censored**");
 	bot.channels.get("282253502779228160").send(":outbox_tray: Left `"+guild.name+"` ("+guild.id+"). Currently in "+bot.guilds.array().length+" servers.");
@@ -26,4 +28,7 @@ exports.run = (guild) => {
 	}, function(error, response, body) {
 		console.log("DBotsList returns success: "+ body);
 	});
+	referreds.no.push(guild.id);
+	referreds.yes.splice(referreds.yes.indexOf(guild.id), 1);
+	fs.writeFile("././json/guilds.json", JSON.stringify(referreds), "utf8");
 };
