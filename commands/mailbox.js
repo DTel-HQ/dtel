06b1dbc032fe4a890c4519428759ebcd68dfedfb
Austin Huang <austinhuang0131@gmail.com>
2017-12-22 21:40:12 -0500

    ok

---
+++ commands/mailbox.js
@@ -0,0 +1,77 @@
+const fs = require("fs");
+var mailbox_storage = JSON.parse(fs.readFileSync("././json/mailbox.json","utf8"));
+
+exports.run = (bot, msg, args) => {
+			var mailbox=mailbox_storage.find(a=>a.channel===msg.channel.id);
+			if(!mailbox){
+				mailbox = {
+					channel:msg.channel.id,
+					settings:{
+					  autoreply:"Sorry I am unavailable, leave a message"
+					},
+					messages:[]
+				};
+				mailbox_storage.push(mailbox);
+				fs.writeFile("mailbox.json",JSON.stringify(mailbox_storage), "utf8");
+			}
+			switch(msg.content.split(" ")[1]){
+				case "settings":
+				if(!msg.content.split(" ")[2]){
+					msg.channel.send({embed:{title: ":tools: Mailbox Settings", description: Object.keys(mailbox.settings).map((a,b)=>a+" `"+mailbox.settings[a]+"`\n*Change the settings with `>mailbox settings [setting name] [value]`*")}});
+				} else {
+					if(mailbox.settings[msg.content.split(" ")[2]]){
+						if (!message.guild.member(message.author).hasPermission("MANAGE_GUILD")) {
+							message.reply("You don't have `Manage Server` permission!");
+						}
+						else {
+							mailbox.settings[msg.content.split(" ")[2]]=msg.content.replace(">mailbox settings "+msg.content.split(" ")[2]+" ","");
+							mailbox_storage[mailbox_storage.indexOf(mailbox_storage.find(a=>a.channel===msg.channel.id))]=mailbox;
+							fs.writeFile("mailbox.json",JSON.stringify(mailbox_storage),function(err){
+								msg.reply((err?err:"Saved."));
+								mailbox_storage=JSON.parse(fs.readFileSync("mailbox.json","utf8"));
+							});
+						}
+					}
+				}
+				break;
+				case "messages":
+				if(!mailbox.messages){
+					msg.channel.send({embed:{title: ":mailbox_with_no_mail: No messages!"}});
+					return;
+				}
+				if(!msg.content.split(" ")[2]){
+					msg.channel.send({embed:{title: ":mailbox_with_mail: You have "+mailbox.messages.length+" messages.", fields: mailbox.messages.map(m=>{
+						return {name: "ID \""+m.id+"\" from "+m.from, value: m.message};
+					}), footer: {text: "Message options: `>mailbox messages [id]`"}}});
+				} else {
+					if(!mailbox.messages.find(a=>a.id===msg.content.split(" ")[2])){
+						msg.channel.send({embed:{title: ":question: I can't find that"}});
+						return;
+					}
+					var message=mailbox.messages.find(a=>a.id===msg.content.split(" ")[2]);
+					switch(msg.content.split(" ")[3]){
+						case "delete":
+						if (!message.guild.member(message.author).hasPermission("MANAGE_GUILD")) {
+							message.reply("You don't have `Manage Server` permission!");
+						}
+						else {
+							mailbox.messages.splice(mailbox.messages.indexOf(message),1);
+							mailbox_storage[mailbox_storage.indexOf(mailbox_storage.find(a=>a.channel===msg.channel.id))]=mailbox;
+							fs.writeFile("mailbox.json",JSON.stringify(mailbox_storage),function(err){
+								msg.reply((err?err:"Deleted!"));
+								mailbox_storage=JSON.parse(fs.readFileSync("mailbox.json","utf8"));
+							});
+						}
+						break;
+						case "callback":
+						msg.reply("`>call "+message.from+"`");
+						break;
+						default:
+						msg.channel.send({embed:{title: ":question: What would you like to do?", description: "`delete` Delete the message\n`callback` Call the caller back", footer: {text: ">mailbox messages "+msg.content.split(" ")[2]+" <Option>"}}});
+					}
+				}
+				break;
+				default:
+				msg.channel.send({embed:{title: ":mailbox: Mailbox", description: (mailbox.messages.length?"**`"+mailbox.messages.length+"` Messages**\n*View them with `>mailbox messages`*\n\n":"")+"**Mailbox Settings**\n"+Object.keys(mailbox.settings).map((a,b)=>a+" `"+mailbox.settings[a]+"`\n*Change the settings with `>mailbox settings`*")}});
+			}
+};
---
+++ commands/message.js
@@ -0,0 +1,40 @@
+const randtoken = require("rand-token"),
+      fs = require("fs");
+var mailbox_storage = JSON.parse(fs.readFileSync("././json/mailbox.json","utf8")),
+	numbers = JSON.parse(fs.readFileSync("././json/numbers.json", "utf8")),
+    accounts = JSON.parse(fs.readFileSync("././json/account.json", "utf8"));
+
+exports.run = (bot, message, args) => {
+	if (args.length !== 3) {
+		message.reply("Correct usage: `>message <Number> <Content>`. 1 message costs 2 credits. Receiving messages is free by using `>mailbox messages`.");
+		return;
+	}
+	var account = accounts.find(function(item) {
+		return item.user === message.author.id;
+	});
+	if (account === undefined) {
+		account = {user:message.author.id,balance:0};
+		accounts.push(account);
+		bot.users.get(message.author.id).send("You don't have an account created...Creating an account for you! Please also read for information on payment: <http://discordtel.readthedocs.io/en/latest/Payment/>");
+	}
+	if (account.balance < 2) {
+		message.reply("Insufficient fund! 1 message costs 2 credits.");
+		return;
+	}
+	accounts.splice(accounts.indexOf(account), 1);
+	account.balance -= 2;
+	accounts.push(account);
+	fs.writeFile("././json/account.json", JSON.stringify(accounts), "utf8");
+	var mailbox=mailbox_storage.find(a=>a.channel===numbers.find(a=>a.number===args[1]).channel);
+	mailbox.messages.push({
+		"id":randtoken.generate(8),
+		"from":numbers.find(a=>a.channel===message.channel.id).number,
+		"message":message.content.replace(">message "+args[1]+" ",""),
+		"callback":true
+	});
+	bot.channels.get(mailbox.channel).send(":mailbox_with_mail: New Message!\n*Check it with `>mailbox messages`*");
+	mailbox_storage[mailbox_storage.indexOf(mailbox_storage.find(a=>a.channel===numbers.find(a=>a.number===args[1]).channel))]=mailbox;
+	fs.writeFile("././json/mailbox.json",JSON.stringify(mailbox_storage),function(err){
+		message.reply((err?err:"Your message is successfully sent."));
+	});
+};
