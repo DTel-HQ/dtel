8427cc50a6355cd898c49e20e6dd52ad00fadd1a
Austin Huang <austinhuang0131@gmail.com>
2017-12-23 16:16:04 -0500

    this is how you do it

--- commands/help.js
+++ commands/help.js
@@ -1,14 +1,72 @@
-const Discord = require("discord.js");
-
-module.exports = async(bot, message, args) => {
-	message.channel.send({embed: new Discord.RichEmbed().setColor("#007FFF").setTitle("List of Commands")
-		.setDescription("For more information, use `>info`.")
-		.addField(">dial / >call", "Dial a number using your own number")
-		.addField(">rdial / >rcall", "Dial a random number in the phonebook (*411)")
-		.addField(">wizard", "Get yourself a number")
-		.addField(">mailbox", "Check your number's mailbox, plus various settings")
-		.addField(">suggest", "Suggest something to be added to the bot")
-		.addField(">convert", "Convert your credits into other bot currency via [Discoin](http://discoin.gitbooks.io/docs)")
-		.addField("Other commands about money", "`>daily`, `>lottery`")
-		.addField("You probably know how these commands work", "`>invite`, `>info`")});
+exports.run = (bot, message, args) => {
+  message.channel.send({
+    embed: {
+      color: 3447003,
+      author: {
+        name: "DiscordTel",
+        icon_url:
+          "https://github.com/austinhuang0131/discordtel/raw/rewrite/discordtel.png",
+        url: "https://discordtel.austinhuang.me"
+      },
+      title: "List of Commands",
+      description: "For more information, use `>info` or `>dial *611`.",
+      fields: [
+        {
+          name: ">dial / >call",
+          value: "Dial a number"
+        },
+        {
+          name: ">rdial / >rcall",
+          value: "Dial a random number from the `*411` phonebook"
+        },
+        {
+          name: ">wizard",
+          value: "Get yourself a number"
+        },
+        {
+          name: ">mailbox",
+          value: "Check mailbox messages, plus various settings"
+        },
+        {
+          name: ">message",
+          value: "Sends a message to another mailbox"
+        },
+        {
+          name: ">convert",
+          value:
+            "Convert your credits into other bot currency via [Discoin](http://discoin.gitbooks.io/docs)"
+        },
+        {
+          name: ">refer",
+          value: "DiscordTel's referral program"
+        },
+        {
+          name: ">mailbox",
+          value: "Read your mailbox messages, plus various settings"
+        },
+        {
+          name: ">convert",
+          value:
+            "Convert your credits into other bot currency via [Discoin](http://discoin.gitbooks.io/docs)"
+        },
+        {
+          name: ">daily",
+          value: "Get daily credits"
+        },
+        {
+          name: ">lottery",
+          value: "Real gambling right there"
+        },
+        {
+          name: "I don't really need to explain them...Right, my master?",
+          value: "`>invite`, `>info`, `>help`"
+        }
+      ],
+      timestamp: new Date(),
+      footer: {
+        icon_url: "https://avatars1.githubusercontent.com/u/16656689?s=460&v=4",
+        text: "Made with <3 by Austin Huang and his team"
+      }
+    }
+  });
 };
