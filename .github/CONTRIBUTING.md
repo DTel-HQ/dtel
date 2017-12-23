8c56484e49d308ccaa9fde06132ead2270271fd3
Austin Huang <austinhuang0131@gmail.com>
2017-12-23 13:57:02 -0500

    Revert "Pr/56"

--- .github/CONTRIBUTING.md
+++
@@ -1,2 +0,0 @@
-* **PR:** Test your code, describe your change, request it.
-* **Issue:** It should be descriptive enough.
--- commands/invite.js
+++
@@ -1,3 +0,0 @@
-module.exports = async(bot, message, args) => {
-	message.reply("https://discordapp.com/oauth2/authorize?client_id=377609965554237453&scope=bot&permissions=67169284\n**All permissions are essential.**");
-};
