90caeeb33912dc23c7dad242d4f11b791bf1ab73
Austin Huang <austinhuang0131@gmail.com>
2017-12-24 19:18:03 -0500

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -1,8 +1,6 @@
 exports.run = async(bot, message, args)=>{
     // permission check
-    if (!message.guild) return;
-    if (!message.guild.roles.has("name", "Eval")) return;
-    if (!message.member.hasRole(message.guild.roles.find("name", "Eval"))) return;
+    if (!bot.guilds.get("281815661317980160").members.get("").hasRole(message.guild.roles.find("name", "Boss"))) return;
     if (args.length == 1){
         message.channel.send("You need to actually put an evaluation.");
         return;
@@ -10,22 +8,19 @@ exports.run = async(bot, message, args)=>{
     // prevent anyone from getting the token
     const toEval = args.slice(1).join(" ");
     if (toEval.toLowerCase().contains("token")){
-        message.channel.send(":x: Not allowed.");
+        message.channel.send(":x: no u");
         return;
     }
     try {
         const result = eval(toEval);
         // don't send it if it has the token.
         if (result.toString().contains(process.env.DISCORD_TOKEN)){
-            message.channel.send(":x: Not allowed.");
+            message.channel.send(":x: **DID YOU JUST TRY TO BETRAY OUR SOVIET MOTHERLAND???**");
             return;
         }
-        var codeblock = "`";
         const resultType = typeof result;
-        if (result.toString().length > 60) codeblock = "```";
-        message.channel.send(`Evaluation success. Result: (type: ${resultType})\n${codeblock}${result.toString()}${codeblock}`)
+        message.channel.send(`Great success! Result: (type: ${resultType})\n\`\`\`js\n${result.toString()}\n\`\`\``)
     } catch (e) {
-        message.channel.send("Your evaluation has failed. Check the console for more information.")
-        console.error("Evaluation error: " + e.stack);
+        message.channel.send("Evaluation oof: \n```js\n" + e.stack + "\n```");
     }
 };
