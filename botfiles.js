c4cff6875f7efd15a82b173e906df3710ec0db65
SunburntRock89 <SunburntRock89@gmail.com>
2018-01-16 23:06:33 +0000

    remove clutter

--- botfiles.js
+++ botfiles.js
@@ -104,16 +104,11 @@ client.on("message", async message => {
 		callDocument = await Calls.findOne({ "to.channelID": message.channel.id });
 		if (!callDocument) throw new Error();
 	} catch (err) {
-		console.log(`no doc there`);
 		try {
-			console.log("2nd try start")
 			// callDocument = await Calls.findOne({{ from: { channelID: message.channel.id } }});
 			callDocument = await Calls.findOne({ "from.channelID": message.channel.id });
-			if (callDocument) console.log(callDocument);
 			if (!callDocument) throw new Error();
-			if (!callDocument) console.log("no call doc 2nd err");
 		} catch (err2) {
-			console.log("errored again, no call doc :(")
 			callDocument = null;
 		}
 	}
