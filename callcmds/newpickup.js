d0df8c7a0644172a442da45394a9c062423d956b
SunburntRock89 <SunburntRock89@gmail.com>
2018-01-16 22:53:28 +0000

    other error

--- callcmds/newpickup.js
+++ callcmds/newpickup.js
@@ -15,7 +15,7 @@ module.exports = async(client, message, callDocument) => {
 		await callDocument.save();
 		return message.reply(":x: The bot has lost permission to send your message to the opposite side. This means the bot could be kicked. Please report this situation to *611, as it could be a troll call.");
 	}
-	toChannel.send(":heavy_check_mark: The other side picked up!");
+	fromChannel.send(":heavy_check_mark: The other side picked up!");
 	client.channels.get(process.env.LOGSCHANNEL).send(`:white_check_mark: The call between channel ${toChannel._id} and channel ${fromChannel._id}} was picked up by __${message.author.tag}__ (${message.author.id}).`);
 	await setTimeout(async() => {
 		if (callDocument.status == false) {
