e3747df69c8634051ff361d24ac484799f5ee153
SunburntRock89 <SunburntRock89@gmail.com>
2018-01-18 20:06:26 +0000

    s h a r d i n g

--- modules/callHandler.js
+++ modules/callHandler.js
@@ -1,19 +1,42 @@
+const MessageBuilder = require("../modules/MessageBuilder");
+
 module.exports = async(client, message, callDocument) => {
-	const support = user_id => client.guilds.get(process.env.SUPPORTGUILD).roles.get(process.env.SUPPORTROLE).members.has(user_id);
-	const donators = user_id => client.guilds.get(process.env.SUPPORTGUILD).roles.get(process.env.DONATORROLE).members.has(user_id);
+	const support = async user => {
+		try {
+			const member = await client.api.guilds(process.env.SUPPORTGUILD).members(user).get();
+			if (member.roles.includes(process.env.SUPPORTROLE)) return true;
+			return false;
+		} catch (err) {
+			console.log(err);
+			return false;
+		}
+	};
+	const donators = async user => {
+		try {
+			const member = await client.api.guilds(process.env.SUPPORTGUILD).members(user).get();
+			if (member.roles.includes(process.env.DONATORROLE)) return true;
+			return false;
+		} catch (err) {
+			console.log(err);
+			return false;
+		}
+	};
 	let sendChannel;
 	if (message.channel.id === callDocument.to.channelID) {
-		sendChannel = client.channels.get(callDocument.from.channelID);
+		sendChannel = callDocument.from.channelID;
 	} else if (message.channel.id === callDocument.from.channelID) {
-		sendChannel = client.channels.get(callDocument.to.channelID);
+		sendChannel = callDocument.to.channelID;
 	} else {
 		message.reply("Error! Please contact a bot developer.");
 	}
+	let send = content => client.channels(sendChannel).messages.post(MessageBuilder({
+		content,
+	}));
 	if (support(message.author.id)) {
-		sendChannel.send(`**${message.author.tag}** :arrow_right: :telephone_receiver: ${message.content}`);
+		send(`**${message.author.tag}** :arrow_right: :telephone_receiver: ${message.content}`);
 	} else if (donators(message.author.id) || message.author.id === `139836912335716352`) {
-		sendChannel.send(`**${message.author.tag}** :arrow_right: <:GoldPhone:320768431307882497> ${message.content}`);
+		send(`**${message.author.tag}** :arrow_right: <:GoldPhone:320768431307882497> ${message.content}`);
 	} else {
-		sendChannel.send(`**${message.author.tag}** :arrow_right: <:DiscordTelPhone:310817969498226718> ${message.content}`);
+		send(`**${message.author.tag}** :arrow_right: <:DiscordTelPhone:310817969498226718> ${message.content}`);
 	}
 };
