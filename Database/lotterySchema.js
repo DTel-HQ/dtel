a59ead9da6cc09d7e0f426ba43d5f57fb82b44e4
SunburntRock89 <SunburntRock89@gmail.com>
2018-02-04 00:08:02 +0000

    god how dumb am I

--- Database/lotterySchema.js
+++ Database/lotterySchema.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 module.exports = new mongoose.Schema({
 	_id: { type: String, required: true },
-	jackpot: { type: String, default: 0 },
+	jackpot: { type: Number, default: 0 },
 	entered: [String],
 	active: { type: Boolean, default: true },
 });
