aa76c3047639a5c339c85eae6b1300120c3bd128
SunburntRock89 <SunburntRock89@gmail.com>
2018-03-29 20:22:10 +0100

    make sure hangup works

--- callcmds/hangup.js
+++ callcmds/hangup.js
@@ -11,16 +11,20 @@ module.exports = async(client, message, args, callDocument) => {
 	message.reply(":negative_squared_cross_mark: You hung up the call.");
 	await client.apiSend(`:negative_squared_cross_mark: The call between channel ${callDocument.from.channelID} and channel ${callDocument.to.channelID} was hung up by __${message.author.tag}__ (${message.author.id}) on the "from" side.`, process.env.LOGSCHANNEL);
 	await client.apiSend(":x: The other side ended the call.", toSend);
-	let hangups = [];
 	try {
-		const fromChannel = await client.api.channels(callDocument.from.channelID).get();
-		hangups.push({ channel: fromChannel.id, guild: fromChannel.guild_id });
-		const toChannel = await client.api.channels(callDocument.to.channelID).get();
-		hangups.push({ channel: toChannel.id, guild: toChannel.guild_id });
-	} catch (err) {
-		console.log(err);
+		let hangups = [];
+		try {
+			const fromChannel = await client.api.channels(callDocument.from.channelID).get();
+			hangups.push({ channel: fromChannel.id, guild: fromChannel.guild_id });
+			const toChannel = await client.api.channels(callDocument.to.channelID).get();
+			hangups.push({ channel: toChannel.id, guild: toChannel.guild_id });
+		} catch (err) {
+			console.log(err);
+		}
+		client.IPC.send("stopTyping", { hangups });
+		await OldCalls.create(new OldCalls(callDocument.toObject()));
+		await callDocument.remove();
+	} catch (_) {
+		// Ignore
 	}
-	client.IPC.send("stopTyping", { hangups });
-	await OldCalls.create(new OldCalls(callDocument.toObject()));
-	await callDocument.remove();
 };
