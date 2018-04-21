a86685b88035ffaa505234d3460b92f8d29e7fc4
Austin Huang <austinhuang0131@gmail.com>
2018-04-21 14:12:38 -0400

    Update callHandler.js

--- modules/callHandler.js
+++ modules/callHandler.js
@@ -28,8 +28,10 @@ module.exports = async(client, message, callDocument) => {
 	});
 	await callDocument.save();
 	setInterval(() => {
-		if (Calls.findOne({ _id: callDocument._id }) === null || Date.now() - callDocument.messages[callDocument.messages.length - 1].time <= 120000) return;
-		client.apiSend(":bulb: Reminder: You still have an ongoing call. You can type `>hangup` to end it.", callDocument.from.channelID);
-		client.apiSend(":bulb: Reminder: You still have an ongoing call. You can type `>hangup` to end it.", callDocument.to.channelID);
+		if (Calls.findOne({ _id: callDocument._id }) === null || Date.now() - callDocument.messages[callDocument.messages.length - 1].time <= 120000) clearInterval();
+		else {
+			client.apiSend(":bulb: Reminder: You still have an ongoing call. You can type `>hangup` to end it.", callDocument.from.channelID);
+			client.apiSend(":bulb: Reminder: You still have an ongoing call. You can type `>hangup` to end it.", callDocument.to.channelID);
+		}
 	}, 300000);
 };
