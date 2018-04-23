6afd092b694b8d7837f3014c1f5cbeeade95837d
Austin Huang <austinhuang0131@gmail.com>
2018-04-23 17:50:11 -0400

    Update pickup.js

--- callcmds/pickup.js
+++ callcmds/pickup.js
@@ -2,9 +2,6 @@ const MessageBuilder = require("../modules/MessageBuilder");
 
 module.exports = async(client, message, args, callDocument) => {
 	if (callDocument.pickedUp) return;
-	callDocument.pickedUp = true;
-	await callDocument.save();
-	message.reply(":white_check_mark: You pick up the call.");
 	let toChannel, fromChannel;
 	try {
 		toChannel = await client.api.channels(callDocument.to.channelID).get();
@@ -13,19 +10,30 @@ module.exports = async(client, message, args, callDocument) => {
 		// Ignore
 	}
 	if (!toChannel || !fromChannel) {
-		callDocument.status = false;
-		await callDocument.save();
+		await callDocument.remove();
 		return message.reply(":x: The bot has lost permission to send your message to the opposite side. This means the bot could be kicked. Please report this situation to *611, as it could be a troll call.");
 	}
-	await client.apiSend(":heavy_check_mark: The other side picked up!", callDocument.from.channelID);
+	try {
+		await client.apiSend(":heavy_check_mark: The other side picked up!", callDocument.from.channelID);
+	} catch(e) {
+		if (e.message === "Missing Permissions") {
+			await callDocument.remove();
+			return message.reply(":x: The bot has lost permission to send your message to the opposite side. This means the bot could be kicked. Please report this situation to *611, as it could be a troll call.");
+		}
+	}
 	await client.apiSend(`:white_check_mark: The call between channel ${toChannel.id} and channel ${fromChannel.id}} was picked up by __${message.author.tag}__ (${message.author.id}).`, process.env.LOGSCHANNEL);
+	callDocument.lastReminder = Date.now();
+	callDocument.pickedUp = true;
+	await callDocument.save();
+	message.reply(":white_check_mark: You pick up the call.");
 	var rem = setInterval(async() => {
-		let call;
-		call = await Calls.findOne({ _id: callDocument._id })
-		if (!call || Date.now() - callDocument.messages[callDocument.messages.length - 1].time <= 120000) clearInterval(rem);
+		callDocument = await Calls.findOne({ _id: callDocument._id });
+		if (!callDocument || Date.now() - callDocument.messages[callDocument.messages.length - 1].time <= 120000 || Date.now() - callDocument.lastReminder < 295000) clearInterval(rem);
 		else {
 			client.apiSend(":bulb: Reminder: You still have an ongoing call ("+callDocument._id+"). You can type `>hangup` to end it.", callDocument.from.channelID);
 			client.apiSend(":bulb: Reminder: You still have an ongoing call ("+callDocument._id+"). You can type `>hangup` to end it.", callDocument.to.channelID);
+			callDocument.lastReminder = Date.now();
+			await callDocument.save();
 		}
 	}, 300000);
 };
