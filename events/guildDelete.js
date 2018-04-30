4418eace0a3de7af46af8f8f955a49196a671b7d
Austin Huang <austinhuang0131@gmail.com>
2018-04-30 08:17:41 -0400

    Update guildDelete.js

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -21,20 +21,5 @@ module.exports = async(client, guild) => {
 		} catch (err) {
 			console.log(`[Shard ${client.shard.id}] Failed to post to DBots`, err);
 		}
-		try {
-			await snekfetch.post(`https://fortunate-fisherman.glitch.me/dtel`)
-				.set(`Authorization`, process.env.BOTS_PW_TOKEN)
-				.set(`Content-Type`, "application/json")
-				.send({
-					shard_id: client.shard.id,
-					shard_count: client.shard.count,
-					server_count: client.guilds.size,
-				})
-				.then(r => { if (!r.body.includes("sun")) {
-					client.user.setActivity(`${r.body} servers | ${process.env.PREFIX}help`);
-				}});
-		} catch (err) {
-			console.log(`[Shard ${client.shard.id}] Failed to post to private glitch server`, err);
-		}
 	}
 };
