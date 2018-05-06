c5a93d91ffa4e20db6c9db5ee08f128b482f62ec
Austin Huang <austinhuang0131@gmail.com>
2018-05-06 12:15:09 -0400

    Update numberSchema.js

--- Database/numberSchema.js
+++ Database/numberSchema.js
@@ -5,6 +5,6 @@ module.exports = new mongoose.Schema({
 	number: { type: String, required: true },
 	expiry: { type: Date, required: true },
 	expired: { type: Boolean, default: false },
-	callerId: { type: String, default: null},
-	callerIdExpiry: { type: Date, default: null }
+	callerId: String,
+	callerIdExpiry: Date
 }, { usePushEach: true });
