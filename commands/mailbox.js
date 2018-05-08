cab129ca71638127b8b9d4bb5987a3cb8da66ed9
Austin Huang <austinhuang0131@gmail.com>
2018-05-08 18:05:54 -0400

    Update mailbox.js

--- commands/mailbox.js
+++ commands/mailbox.js
@@ -89,7 +89,7 @@ module.exports = async(client, msg, args) => {
 			break;
 		}
 		case "delete": {
-			let message = mailbox.messages.find(m => m._id === args.split(" ")[2]);
+			let message = mailbox.messages.find(m => m._id === args.split(" ")[1]);
 			if (!message) {
 				return msg.channel.send({
 					embed: {
