e146becc2077e428513759ba37ec8bc94014732c
Austin Huang <austinhuang0131@gmail.com>
2018-05-11 16:55:15 -0400

    How to bamboozle other people

---
+++ commands/kiss.js
@@ -0,0 +1,3 @@
+module.exports = async(client, message, args) => {
+	message.channel.send("Wonder what? I saw you guys kiss. Don't try to cheat off me, <@"+message.author.id+">! You only have one true love. That's me.");
+};
