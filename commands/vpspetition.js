caa45399bbd96250acbd290bffb549c0f823517b
Charalampos Fanoulis <38255093+cfanoulis@users.noreply.github.com>
2018-08-20 18:40:44 +0300

    Codacy I hate you
    but doublequotes still rock

--- commands/vpspetition.js
+++ commands/vpspetition.js
@@ -1,3 +1,3 @@
 module.exports = async(client, message, args) => {
-	message.channel.send(`Help us give Austin VPS access by signing our petition. :arrow_right: <https://secure.avaaz.org/en/petition/SunburntRock896617_Give_Austin_VPS_access/?cztCckb>`);
+	message.channel.send("Help us give Austin VPS access by signing our petition. :arrow_right: <https://secure.avaaz.org/en/petition/SunburntRock896617_Give_Austin_VPS_access/?cztCckb>");
 };
