d4d1177ea8e1198d4e05069aa15412f6f2115def
Austin Huang <im@austinhuang.me>
2018-08-24 22:11:08 -0400

    Update daily.js

--- commands/daily.js
+++ commands/daily.js
@@ -19,34 +19,9 @@ module.exports = async(client, msg, suffix) => {
 	} else if (perms.support) {
 		toGive = 200;
 	}
-	if (!toGive) {
-		/* let snekres;
-		try {
-			snekres = await get("https://discordbots.org/api/bots/377609965554237453/votes?onlyids=true").set({ "content-type": "application/json", Authorization: process.env.DBL_ORG_TOKEN });
-		} catch (err) {
-			msg.reply("Could not reach discordbots.org. Please try again later");
-		}
-		if (snekres) {
-			if (snekres.body.includes(msg.author.id)) {
-				if (perms.support) {
-					toGive = 260;
-				} else {
-					toGive = 180;
-				}
-			}
-		}*/
-	}
-	if (toGive) {
-		account.balance += toGive;
-		account.dailyClaimed = true;
-		await account.save();
-		await client.apiSend(`:calendar: ${msg.author.tag} (${msg.author.id}) claimed ${toGive} daily credits.`, process.env.LOGSCHANNEL);
-		if (toGive == 120) {
-			msg.reply("Here's your 120 credits!");
-		} else if (toGive >= 180) {
-			msg.reply(`Here's your ${toGive} credits!`);
-		}
-	} else {
-		msg.reply("Catastrophic Failure");
-	}
+	account.balance += toGive;
+	account.dailyClaimed = true;
+	await account.save();
+	await client.apiSend(`:calendar: ${msg.author.tag} (${msg.author.id}) claimed ${toGive} daily credits.`, process.env.LOGSCHANNEL);
+	msg.reply(`Here's your ${toGive} credits!`);
 };
