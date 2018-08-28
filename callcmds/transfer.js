0bc128c7f912001ce32b669663862ec405a39276
Austin Huang <im@austinhuang.me>
2018-08-27 21:10:31 -0400

    Update transfer.js

--- callcmds/transfer.js
+++ callcmds/transfer.js
@@ -96,15 +96,15 @@ module.exports = async (client, message, args, callDocument) => {
     await client.apiSend(`:arrow_right: A call in channel ${message.channel.id} has been transferred to ${toDialDocument._id} by __${message.author.tag}__ (${message.author.id}).`, process.env.LOGSCHANNEL);
     let toSend;
     callDocument.pickedUp = false;
-    callDocument.onHold = false;
+    callDocument.onHold = "";
     if (callDocument.to.channelID === message.channel.id) {
         toSend = callDocument.from.channelID;
         await client.apiSend(`There is an incoming call (**Transferred** from \`${callDocument.to.number}\`) from \`${callDocument.from.number}\`. You can either type \`>pickup\` or \`>hangup\`, or wait it out.`, toDialDocument._id);
         callDocument.transfer = true;
-        callDocument.from = {channelID: callDocument.to._id, number: callDocument.to.number};
     } else {
         toSend = callDocument.to.channelID;
         await client.apiSend(`There is an incoming call (**Transferred** from \`${callDocument.from.number}\`) from \`${callDocument.to.number}\`. You can either type \`>pickup\` or \`>hangup\`, or wait it out.`, toDialDocument._id);
+        callDocument.from = {channelID: callDocument.to._id, number: callDocument.to.number};
     }
     callDocument.to = {channelID: toDialDocument._id, number: toDialDocument.number};
     await client.apiSend(`:arrow_right: Your call has been transferred to \`${toDial}\`, awaiting pickup. You are able to \`>hangup\`.`, toSend);
