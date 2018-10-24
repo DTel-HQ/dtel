4416a17e523b8e0e8caedeb99b841dec262ebeec
Mitchell3514 <m.d.rademaker19@gmail.com>
2018-10-24 21:29:04 +0200

    Make it display the actual side.

--- callcmds/hangup.js
+++ callcmds/hangup.js
@@ -3,13 +3,16 @@ const MessageBuilder = require("../modules/MessageBuilder");
 module.exports = async(client, message, args, callDocument) => {
 	if (!callDocument) return console.log("wtf no calldoc", callDocument);
 	let toSend;
+	let side; //rename
 	if (callDocument.to.channelID === message.channel.id) {
 		toSend = callDocument.from.channelID;
+		side = 'from';
 	} else {
 		toSend = callDocument.to.channelID;
+		side = 'to';
 	}
 	message.reply(":negative_squared_cross_mark: You hung up the call.");
-	await client.apiSend(`:negative_squared_cross_mark: The call between channel ${callDocument.from.channelID} and channel ${callDocument.to.channelID} was hung up by __${message.author.tag}__ (${message.author.id}) on the "from" side.`, process.env.LOGSCHANNEL);
+	await client.apiSend(`:negative_squared_cross_mark: The call between channel ${callDocument.from.channelID} and channel ${callDocument.to.channelID} was hung up by __${message.author.tag}__ (${message.author.id}) on the ${side} side.`, process.env.LOGSCHANNEL);
 	await client.apiSend(":x: The other side ended the call.", toSend);
 	try {
 		let hangups = [];
