46cfbf25622bc61b30f890bc5d05849e61fe7bb7
Austin Huang <austinhuang0131@gmail.com>
2018-11-14 21:59:22 -0500

    Update wizard.js

--- commands/wizard.js
+++ commands/wizard.js
@@ -22,12 +22,32 @@ module.exports = async(client, message, args) => {
 	if (guildnumber) {
 		return message.reply(":x: Error: You already have a number in this guild. If you wish to edit, remove your number or add another number, dial `*611`.");
 	} else if (!message.guild) {
-		message.reply(`**WARNING: You're about to register a DM number for yourself.**\n\nPlease read the following before proceeding.\n\`\`\`diff\n+ By going through the wizard you'll enable DiscordTel service in THIS channel.\n- You are required to read and fully understand the documentation, including important payment information which is required to renew your number. The documentation is available at http://discordtel.rtfd.io.\n+ Your usage in the current calendar month is free.\n- Any abuse on our system will cause termination of service.\n\`\`\`\nPlease enter the number you wish to enable in <#${message.channel.id}>. The number must start with \`0900\` followed by another 7 digits. Type \`0\` to quit the wizard.`);
-	} else if (!message.member.hasPermission("MANAGE_GUILD")) {
+		message.channel.send({embed: {
+			color: 0x007FFF,
+			title: "Before proceeding, please read the following information.",
+			fields: [
+				{name: "This is a roleplay bot!",
+				 value: "In case you haven't noticed, this bot is a roleplay bot used to simulate a telephone system between Discord servers, which means it **cannot call real numbers!!!**"
+				},
+				{name: "Documentation",
+				 value: "The documentation is located at https://discordtel.austinhuang.me. **Please read it** as it contains important information regarding the use of this bot."
+				},
+				{name: "Payment",
+				 value: "**Your number must be renewed for every month of usage.** The number will cost **500** in-bot credits per month, after 1 month of free period. You must type `>dial *411` to renew it. See [here](https://discordtel.austinhuang.me/en/latest/Payment/) for ways to get credits. **No real-money purchase is required to use this bot and renew your number.**"
+				},
+				{name: "You're registering a number in DM!",
+				 value: "...which means all calls to the number will show up as direct messages. Just to let you know."
+				},
+				{name: "I'm ready!",
+				 value: `Please enter the number you wish to enable in <#${message.channel.id}>. The number must start with \`030${prefix}\` followed by another 7 digits. Type \`0\` to quit the wizard.`
+				}
+			]
+		}});	} else if (!message.member.hasPermission("MANAGE_GUILD")) {
 		return message.reply("Self-assign wizard can only be run with a member that has Manage Server permission.");
 	} else {
 		let prefix = (client.shard.id + 1).toString();
-		message.reply({embed: {
+		message.channel.send({embed: {
+			color: 0x007FFF,
 			title: "Before proceeding, please read the following information.",
 			fields: [
 				{name: "This is a roleplay bot!",
