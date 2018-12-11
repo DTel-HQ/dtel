b7e968b5d734f20dd664e822223eb6ac5d26da5d
Austin Huang <austinhuang0131@gmail.com>
2018-12-11 15:35:20 -0500

    Update accountSchema.js

--- Database/accountSchema.js
+++ Database/accountSchema.js
@@ -4,6 +4,6 @@ module.exports = new mongoose.Schema({
 	_id: { type: String, required: true },
 	balance: { type: Number, default: 0 },
 	dailyClaimed: { type: Boolean, default: false },
-	vip: {type: Number, default: 0}, // VIP expiry
+	vip: Date, // VIP expiry
 	premium: String, // Which number is assigned VIP
 }, { usePushEach: true });
