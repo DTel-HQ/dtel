fd4b23faa509506464e822a92c622ab19100e052
Rexogamer <beartechtalks@gmail.com>
2019-06-07 09:59:52 +0100

    grammar
    also I’d like to add `bal` as an alias but I dunno how to with this bot

--- commands/balance.js
+++ commands/balance.js
@@ -8,7 +8,7 @@ module.exports = async(client, msg, suffix) => {
 			await Accounts.create(new Accounts({
 				_id: msg.author.id,
 			}));
-			msg.reply("You don't have an account created...Creating an account for you! Please also read for information on payment: <http://discordtel.readthedocs.io/en/latest/Payment/>");
+			msg.reply("You don't have an DiscordTel account... creating one for you! Please also read this for information on payment: <http://discordtel.readthedocs.io/en/latest/Payment/>");
 			account = await Accounts.findOne({ _id: msg.author.id });
 		}
 		msg.channel.send({
