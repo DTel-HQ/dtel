fa265b84d3c928e303f1dc5c8cf67a8e323920fc
Mitchell3514 <10900096+Mitchell3514@users.noreply.github.com>
2019-08-02 21:49:19 +0200

    respond?

--- commands/pull.js
+++ commands/pull.js
@@ -3,6 +3,6 @@ module.exports = async(client, msg, suffix) => {
   const perms = await client.permCheck(msg.author.id);
   if (perms.boss || ["139836912335716352", "124989722668957700"].contains(msg.author.id)) {
     await exec("pm2 pull 0");
-    return true;
+    return msg.reply("Success... probably");
   }
 };
