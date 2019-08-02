5ca854640cf1ae7e142e64ab71b3717e200afa0d
Mitchell3514 <m.d.rademaker19@gmail.com>
2019-08-02 16:54:58 +0200

    Remove unneeded requires

--- Commands/Private/pull.js
+++ Commands/Private/pull.js
@@ -1,7 +1,4 @@
 const { exec } = require("child_process");
-const { readdir } = require("fs-nextra");
-const clear = require("clear-module");
-
 
 module.exports = async(client, msg, suffix) => {
 	exec("git pull");
