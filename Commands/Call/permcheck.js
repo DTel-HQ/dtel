99d5c0e6fc7f9625617d6443a75f8daae7a60262
Mitchell3514 <m.d.rademaker19@gmail.com>
2019-08-03 00:29:19 +0200

    Fixes permcheck

--- Commands/Call/permcheck.js
+++ Commands/Call/permcheck.js
@@ -1,9 +1,10 @@
 module.exports = async(client, msg, suffix) => {
 	let perms = await msg.author.getPerms();
-	if (!perms.support || msg.channel.id != config.supportChannel || !call.pickedUp) return;
 
 	let call = await Calls.find(c => c.to.channel === msg.channel.id || c.from.channel === msg.channel.id);
 	if (!call) return;
+
+	if (!perms.support || msg.channel.id != config.supportChannel || !call.pickedUp) return;
 
 	let channel = await client.channels.get(call.to.channel === config.supportChannel ? call.from.channel : call.to.channel);
 	if (!channel) return msg.reply("Couldn't find the other side");
