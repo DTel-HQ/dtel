194eee2183944b3eabfb46812ba6086dad2b8013
Mitchell3514 <10900096+Mitchell3514@users.noreply.github.com>
2019-08-02 21:50:30 +0200

    The old way

--- commands/pull.js
+++ commands/pull.js
@@ -2,7 +2,7 @@ const { exec } = require("child_process");
 module.exports = async(client, msg, suffix) => {
   const perms = await client.permCheck(msg.author.id);
   if (perms.boss || ["139836912335716352", "124989722668957700"].contains(msg.author.id)) {
-    await exec("pm2 pull 0");
+    await exec("git pull && pm2 restart 0");
     return msg.reply("Success... probably");
   }
 };
