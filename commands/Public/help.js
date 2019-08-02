b58ea7ef7b335d88d8748a32d1b31b8a056ebf08
Mitchell3514 <m.d.rademaker19@gmail.com>
2019-08-02 14:20:35 +0200

    ^

--- commands/Public/help.js
+++ commands/Public/help.js
@@ -67,7 +67,7 @@ module.exports = async(client, msg, suffix) => {
 			],
 			timestamp: new Date(),
 			footer: {
-				icon_url: "https://avatars1.githubusercontent.com/u/16656689?s=460&v=4",
+				icon_url: "https://avatars1.githubusercontent.com/u/16656689",
 				text: "DiscordTel V3 - made with <3 by Austin Huang and his team",
 			},
 		},
