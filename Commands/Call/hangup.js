714ae5a94c8be21c4da83757668fe3301faae1ce
Mitchell3514 <m.d.rademaker19@gmail.com>
2019-08-03 20:34:43 +0200

    beautify logs

--- Commands/Call/hangup.js
+++ Commands/Call/hangup.js
@@ -6,7 +6,7 @@ module.exports = async(client, msg, suffix) => {
 	await r.table("OldCalls").insert(call);
 	await Calls.newGet(call.id).delete();
 	client.apiSend(":x: The other side hung up the call.", msg.channel.id === call.from.channel ? call.to.channel : call.from.channel);
-	client.log(`:negative_squared_cross_mark: ${call.rcall ? "Rcall" : "call"} \`${call.from.channel} → ${call.to.channel}\` was hung up by ${msg.author.tag} (${msg.author.id}) on the ${msg.channel.id === call.from.channel ? "from" : "to"} side. ||${call.id}||`);
+	client.log(`:negative_squared_cross_mark: ${call.rcall ? "Rcall" : "Call"} \`${call.from.channel} → ${call.to.channel}\` was hung up by ${msg.author.tag} (${msg.author.id}) on the ${msg.channel.id === call.from.channel ? "from" : "to"} side. ${call.id}`);
 
 	// add stop typing things later?
 };
