dd0f7fa8523aeb3bce4ba8dec8f4a3d923c17f01
Mitchell3514 <m.d.rademaker19@gmail.com>
2019-08-03 16:36:48 +0200

    Beautify pull

--- Commands/Private/pull.js
+++ Commands/Private/pull.js
@@ -2,5 +2,5 @@ const { exec } = require("child_process");
 
 module.exports = async(client, msg, suffix) => {
 	await exec("git pull");
-	return msg.reply("Done");
+	return msg.channel.send({ embed: { color: 0xEEEEEE, title: "Executed Git pull", description: "If there was an update to the following, use `>resart:`\n•Config\n•Structures" } });
 };
