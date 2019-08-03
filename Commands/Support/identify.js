9cccdb92f3c7c93954f4d270525d2d63707500a6
Mitchell3514 <m.d.rademaker19@gmail.com>
2019-08-03 16:02:01 +0200

    sun fix

--- Commands/Support/identify.js
+++ Commands/Support/identify.js
@@ -2,14 +2,14 @@ const { MessageEmbed } = require("discord.js");
 module.exports = async(client, msg, suffix) => {
 	const callID = suffix.split(" ")[0];
 	const msgID = suffix.split(" ")[1];
-	if (!callID || !msgID) return msg.channel.send({ embed: { color: 0x006, title: "Command usage", description: ">identify [call ID] [Message ID]\nReturns with the user ID of the sender." } });
+	if (!callID || !msgID) return msg.channel.send({ embed: { color: 0x000066, title: "Command usage", description: ">identify [call ID] [Message ID]\nReturns with the user ID of the sender." } });
 
 	let call = await r.table("OldCalls").get(callID);
 	if (!call) call = Calls.get(callID);
-	if (!call) return msg.channel.send({ embed: { color: 0x600, title: "Call not found", description: "Check if you have the right call ID." } });
+	if (!call) return msg.channel.send({ embed: { color: 0x660000, title: "Call not found", description: "Check if you have the right call ID." } });
 
-	if (!call.messages) return msg.channel.send({ embed: { color: 0x600, title: "No messages", description: "This call does not contain any messages." } });
+	if (!call.messages) return msg.channel.send({ embed: { color: 0x660000, title: "No messages", description: "This call does not contain any messages." } });
 	let msgDoc = await call.messages.find(m => m.dtelmsg == msgID);
-	if (!msgDoc) return msg.channel.send({ embed: { color: 0x600, title: "Message not found", description: "Couldn't find that message in the call.\nMake sure the message comes from DTel." } });
-	return msg.channel.send({ embed: { color: 0x600, title: "Message found!", description: `User: ${msgDoc.user}\nTime: ${msgDoc.time}` } });
+	if (!msgDoc) return msg.channel.send({ embed: { color: 0x660000, title: "Message not found", description: "Couldn't find that message in the call.\nMake sure the message comes from DTel." } });
+	return msg.channel.send({ embed: { color: 0x006600, title: "Message found!", description: `User: <@${msgDoc.user}>(${msgDoc.user})` } });
 };
