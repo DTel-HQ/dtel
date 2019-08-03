aa005f1544dcbb27052b530688fa8488afc053a0
Mitchell3514 <m.d.rademaker19@gmail.com>
2019-08-04 00:45:41 +0200

    Fixes cinfo

--- Commands/Support/cinfo.js
+++ Commands/Support/cinfo.js
@@ -1,7 +1,7 @@
 const { MessageEmbed } = require("discord.js");
 module.exports = async(client, msg, suffix) => {
 	let call = Calls.get(suffix);
-	if (!call) call = await r.table("Calls").get(suffix);
+	if (!call) call = await r.table("OldCalls").get(suffix);
 	if (!call) return msg.channel.send({ embed: { color: 0x660000, title: "No call", description: "Couldn't find a call with that ID" } });
 
 	let embed = new MessageEmbed()
