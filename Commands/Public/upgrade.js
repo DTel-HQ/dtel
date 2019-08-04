100e7130fa8fb81e5de161831742230bcac86e6e
Mitchell3514 <m.d.rademaker19@gmail.com>
2019-08-04 21:59:48 +0200

    upgrade fix for no number

--- Commands/Public/upgrade.js
+++ Commands/Public/upgrade.js
@@ -3,7 +3,7 @@ const { MessageEmbed } = require("discord.js");
 module.exports = async(client, msg, suffix) => {
 	// Get their details
 	let number = (await r.table("Numbers").filter({ channel: msg.channel.id }))[0];
-	let vipExpiry = number.vip ? new Date(number.vip.expiry) : null;
+	let vipExpiry = number ? number.vip ? new Date(number.vip.expiry) : null : null;
 	let vipNumber = vipExpiry ? vipExpiry.getTime() > Date.now() : false;
 
 	let account = await r.table("Accounts").get(msg.author.id);
