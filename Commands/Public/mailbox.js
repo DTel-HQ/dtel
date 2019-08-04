a8cc76a9adc9ae010758f1840aa6bcc7b68f83da
Mitchell3514 <m.d.rademaker19@gmail.com>
2019-08-04 22:58:46 +0200

    . . .

--- Commands/Public/mailbox.js
+++ Commands/Public/mailbox.js
@@ -159,7 +159,7 @@ module.exports = async(client, msg, suffix) => {
 
 			collected = (await msg.channel.awaitMessages(
 				m => m.author.id == msg.author.id && (m.content === "0" || responses.includes(m.content.toLowerCase()) || (parseInt(m.content) != page && parseInt(m.content) > 0 && parseInt(m.content) <= pages) || messages.filter(message => message.id == m.content).length > 0),
-				{	time: 120000 })).first();
+				{	time: 120000, max: 1 })).first();
 
 			if (collected) collected.delete().catch(e => null);
 			switch (collected.content) {
