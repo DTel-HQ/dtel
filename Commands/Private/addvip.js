a1bef05a8f5701ef143f389a715c1b59324d5193
Mitchell3514 <m.d.rademaker19@gmail.com>
2019-08-05 02:56:46 +0200

    Making addvip better

--- Commands/Private/addvip.js
+++ Commands/Private/addvip.js
@@ -1,7 +1,7 @@
 module.exports = async(client, msg, suffix) => {
 	let user = msg.mentions.users ? msg.mentions.users.first() : client.users.fetch(suffix.split(" ")[0]);
 	let months = suffix.split(" ")[1];
-	if (!user || !months) return msg.channel.send({ embed: { color: 0xEEEEEE, title: "Command usage", description: ">addvip [user] [months]" } });
+	if (!user || !months || months == 0) return msg.channel.send({ embed: { color: 0xEEEEEE, title: "Command usage", description: ">addvip [user] [months]" } });
 	if (!Number(months)) return msg.channel.send({ embed: { color: 0x660000, title: "Wrong usage", description: "Months needs to be a number" } });
 
 	let account = await r.table("Accounts").get(user.id);
@@ -15,6 +15,7 @@ module.exports = async(client, msg, suffix) => {
 
 	await r.table("Accounts").get(account.id).update({ vip: account.vip ? account.vip + Number(months) : Number(months) });
 	client.log(`💠 User ${user.tag} (${user.id}) has purchased and received ${months} VIP Months`);
-	msg.channel.send({ embed: { color: 0xEEEEEE, title: "Succes", description: `Added \`${months}\` VIP Months to ${user}(${user.id})`, footer: { text: `Executed by: ${msg.author}(${msg.author.id})` } } });
-	(await user.createDM()).send({ embed: { color: 0xEEEEEE, title: "Thanks for your purchase!", description: `You have received \`${months}\` VIP Months`, footer: { text: `Executed by: ${msg.author.tag}(${msg.author.id})` } } }).catch(e => null);
+	msg.channel.send({ embed: { color: 0xEEEEEE, title: "Succes", description: `${Number(months) > 0 ? "Added" : "Removed"} \`${Number(months) > 0 ? months : months * -1}\` VIP Months to ${user}(${user.id})`, footer: { text: `Executed by: ${msg.author}(${msg.author.id})` } } });
+	if (Number(months) > 0) (await user.createDM()).send({ embed: { color: 0xEEEEEE, title: "Thanks for your purchase!", description: `You have received \`${months}\` VIP Months.`, footer: { text: `Executed by: ${msg.author.tag}(${msg.author.id})` } } }).catch(e => null);
+	else (await user.createDM()).send({ embed: { color: 0xEEEEEE, title: "VIP Months have been removed", description: `There have been \`${months * -1}\` VIP Months removed from your account.`, footer: { text: `Executed by: ${msg.author.tag}(${msg.author.id})` } } }).catch(e => null);
 };
