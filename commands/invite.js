08943f42ee53a2cd6977875161d8956beb294f38
Mitchell3514 <10900096+Mitchell3514@users.noreply.github.com>
2019-09-06 10:55:08 +0200

    ..

--- commands/invite.js
+++ commands/invite.js
@@ -1,3 +1,3 @@
 module.exports = async(client, message, args) => {
-	message.reply("https://discordapp.com/oauth2/authorize?client_id=377609965554237453&scope=bot&permissions=67169284\n**All permissions are essential.**");
+	message.reply("Bot: https://discordapp.com/oauth2/authorize?client_id=377609965554237453&scope=bot&permissions=67169284\n**All permissions are essential.**\nSupport server: https://discord.gg/qRVxY55");
 };
