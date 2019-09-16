cbe62253052aeea226a54f4913868acbe2d1568d
Mitchell3514 <10900096+Mitchell3514@users.noreply.github.com>
2019-09-16 12:33:03 +0200

    Getting rid of dupe

--- Structures/DTelChannels.js
+++ Structures/DTelChannels.js
@@ -1,8 +1,8 @@
-const { Structures } = require("discord.js");
+const { Structures, Channel } = require("discord.js");
 
 module.exports = () => {
-	Structures.extend("TextChannel", TextChannel => {
-		class DTelChannel extends TextChannel {
+	let extend = Structure => {
+		class DTelChannel extends Structure {
 			get number() {
 				return r.table("Numbers").getAll(this.id, { index: "channel" }).nth(0).default(null);
 			}
@@ -16,22 +16,8 @@ module.exports = () => {
 			}
 		}
 		return DTelChannel;
-	});
+	};
 
-	Structures.extend("DMChannel", DMChannel => {
-		class DTelChannel extends DMChannel {
-			get number() {
-				return r.table("Numbers").getAll(this.id, { index: "channel" }).nth(0).default(null);
-			}
-
-			get call() {
-				return (async() => {
-					let call = await r.table("Calls").getAll(this.id, { index: "fromChannel" }).nth(0).default(null);
-					if (!call) call = await r.table("Calls").getAll(this.id, { index: "toChannel" }).nth(0).default(null);
-					return call;
-				})();
-			}
-		}
-		return DTelChannel;
-	});
+	Structures.extend("TextChannel", TextChannel => extend(TextChannel));
+	Structures.extend("DMChannel", DMChannel => extend(DMChannel));
 };
