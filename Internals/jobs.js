903c92b6d17155cfead5c9c81cd6c554be5bc489
Mitchell3514 <10900096+Mitchell3514@users.noreply.github.com>
2019-09-21 15:56:26 +0200

    Don't run this on launch

--- Internals/jobs.js
+++ Internals/jobs.js
@@ -61,81 +61,81 @@ scheduleJob("*/15 * * * * *", async() => {
 
 
 // Job to delete numbers if expired for a long time
-scheduleJob("0 0 0 * * *", async() => {
-	// Don't just change lastwarn k
-	const warnDays = 15;
-	const lastWarn = 29;
-	const deleteDays = 30;
-
-	let time = Date.now();
-	const warnMS = time - (warnDays * 86400000);
-	const lastWarnMS = time - (lastWarn * 86400000);
-	const deleteMS = time - (deleteDays * 86400000);
-
-	if (client.shard.ids[0] != 0) return;
-
-	const numbers = await r.table("Numbers");
-	let deleted = [];
-
-	for (let number of numbers) {
-		let channel = await client.api.channels(number.channel).get();
-		if (!channel) {
-			await client.delete(number, { force: true, stopLog: true });
-			deleted.push(number);
-			break;
-		}
-		let owner = number.guild ? (await client.api.guilds(number.guild).get().catch(e => null)).owner_id : null;
-
-		let embed = new MessageEmbed()
-			.setColor(config.colors.error);
-
-		let otitle,
-			odesc,
-			ctitle,
-			cdesc;
-
-		let expiryMS = new Date(number.expiry).getTime();
-
-		if (expiryMS < deleteMS) {
-			await client.delete(number, true);
-			deleted.push(number);
-
-			otitle = `Your number has been deleted`;
-			odesc = `Your number (${number.id}) in <#${channel.id}> has been deleted as it has been expired for >${deleteDays} days.`;
-			ctitle = `This channel's number has been deleted`;
-			cdesc = `This channel's number (${number.id}) has been deleted as it has been expired for >${deleteDays} days.`;
-		} else if (expiryMS < warnMS && expiryMS > warnMS - 86400000) {
-			otitle = `Your number soon be deleted.`;
-			odesc = `Your number ${number.id} in <#${channel.id}> has expired and will be automatically deleted in ${deleteDays - warnDays} days. To prevent losing your number (and all that comes with it), please extend the duration of your number by calling \`*233\`. `;
-			ctitle = `This number will soon be deleted`;
-			cdesc = `This channel's number (${number.id}) has been expired for >${warnDays} days and will automatically be deleted in ${deleteDays - warnDays}. To prevent losing this number and all its settings, please extend it by calling \`*233\`.`;
-		} else if (expiryMS < lastWarnMS && expiryMS > lastWarnMS - 86400000) {
-			otitle = `❕This number will be deleted in 24h❕`;
-			odesc = `Your number ${number.id} in <#${channel.id}> has been expired for >${lastWarn} days and will automatically be deleted in **24h**. To prevent losing your number (and all that comes with it), please extend the duration of your number by calling \`*233\`. `;
-			ctitle = `❕This number will be deleted in 24h❕`;
-			cdesc = `This channel's number (${number.id}) has been expired for >${lastWarn} days and will automatically be deleted in **24h**. To prevent losing this number and all its settings, please extend it by calling \`*233\`.`;
-		} else {
-			continue;
-		}
-
-		embed.setTitle(ctitle)
-			.setDescription(cdesc);
-		await client.apiSend({ embed: embed }, channel.id).catch(e => null);
-		embed.setTitle(otitle)
-			.setDescription(odesc)
-			.setFooter("You are receiving this as you are the owner of the server.");
-		await (await client.users.fetch(owner)).send({ embed: embed }).catch(e => null);
-	}
-
-	let haste;
-	try {
-		haste = await post("https://hastebin.com/documents").send(`// Deleted numbers: ${new Date()}\n\n${deleted.map(n => `${n.id}\n`)}`);
-	} catch (err) {
-		haste = false;
-	}
-
-	let count = deleted.length;
-	winston.info(`[ScheduleJob] Deleted ${count} expired numbers`);
-	if (haste) client.log(`🔥 Automatically deleted ${count} numbers. Results: <https://hastebin.com/${haste.body.key}>`);
-	else client.log(`🔥 Automatically deleted ${count} numbers. Unsuccesful post to hastebin.`);
-});
+// scheduleJob("0 0 0 * * *", async() => {
+// 	// Don't just change lastwarn k
+// 	const warnDays = 15;
+// 	const lastWarn = 29;
+// 	const deleteDays = 30;
+//
+// 	let time = Date.now();
+// 	const warnMS = time - (warnDays * 86400000);
+// 	const lastWarnMS = time - (lastWarn * 86400000);
+// 	const deleteMS = time - (deleteDays * 86400000);
+//
+// 	if (client.shard.ids[0] != 0) return;
+//
+// 	const numbers = await r.table("Numbers");
+// 	let deleted = [];
+//
+// 	for (let number of numbers) {
+// 		let channel = await client.api.channels(number.channel).get();
+// 		if (!channel) {
+// 			await client.delete(number, { force: true, stopLog: true });
+// 			deleted.push(number);
+// 			break;
+// 		}
+// 		let owner = number.guild ? (await client.api.guilds(number.guild).get().catch(e => null)).owner_id : null;
+//
+// 		let embed = new MessageEmbed()
+// 			.setColor(config.colors.error);
+//
+// 		let otitle,
+// 			odesc,
+// 			ctitle,
+// 			cdesc;
+//
+// 		let expiryMS = new Date(number.expiry).getTime();
+//
+// 		if (expiryMS < deleteMS) {
+// 			await client.delete(number, true);
+// 			deleted.push(number);
+//
+// 			otitle = `Your number has been deleted`;
+// 			odesc = `Your number (${number.id}) in <#${channel.id}> has been deleted as it has been expired for >${deleteDays} days.`;
+// 			ctitle = `This channel's number has been deleted`;
+// 			cdesc = `This channel's number (${number.id}) has been deleted as it has been expired for >${deleteDays} days.`;
+// 		} else if (expiryMS < warnMS && expiryMS > warnMS - 86400000) {
+// 			otitle = `Your number soon be deleted.`;
+// 			odesc = `Your number ${number.id} in <#${channel.id}> has expired and will be automatically deleted in ${deleteDays - warnDays} days. To prevent losing your number (and all that comes with it), please extend the duration of your number by calling \`*233\`. `;
+// 			ctitle = `This number will soon be deleted`;
+// 			cdesc = `This channel's number (${number.id}) has been expired for >${warnDays} days and will automatically be deleted in ${deleteDays - warnDays}. To prevent losing this number and all its settings, please extend it by calling \`*233\`.`;
+// 		} else if (expiryMS < lastWarnMS && expiryMS > lastWarnMS - 86400000) {
+// 			otitle = `❕This number will be deleted in 24h❕`;
+// 			odesc = `Your number ${number.id} in <#${channel.id}> has been expired for >${lastWarn} days and will automatically be deleted in **24h**. To prevent losing your number (and all that comes with it), please extend the duration of your number by calling \`*233\`. `;
+// 			ctitle = `❕This number will be deleted in 24h❕`;
+// 			cdesc = `This channel's number (${number.id}) has been expired for >${lastWarn} days and will automatically be deleted in **24h**. To prevent losing this number and all its settings, please extend it by calling \`*233\`.`;
+// 		} else {
+// 			continue;
+// 		}
+//
+// 		embed.setTitle(ctitle)
+// 			.setDescription(cdesc);
+// 		await client.apiSend({ embed: embed }, channel.id).catch(e => null);
+// 		embed.setTitle(otitle)
+// 			.setDescription(odesc)
+// 			.setFooter("You are receiving this as you are the owner of the server.");
+// 		await (await client.users.fetch(owner)).send({ embed: embed }).catch(e => null);
+// 	}
+//
+// 	let haste;
+// 	try {
+// 		haste = await post("https://hastebin.com/documents").send(`// Deleted numbers: ${new Date()}\n\n${deleted.map(n => `${n.id}\n`)}`);
+// 	} catch (err) {
+// 		haste = false;
+// 	}
+//
+// 	let count = deleted.length;
+// 	winston.info(`[ScheduleJob] Deleted ${count} expired numbers`);
+// 	if (haste) client.log(`🔥 Automatically deleted ${count} numbers. Results: <https://hastebin.com/${haste.body.key}>`);
+// 	else client.log(`🔥 Automatically deleted ${count} numbers. Unsuccesful post to hastebin.`);
+// });
