541c087931c6387c129fe351abd55c2352f264ec
Mitchell3514 <10900096+Mitchell3514@users.noreply.github.com>
2019-09-22 21:06:01 +0200

    Update balance.js

--- Commands/Public/balance.js
+++ Commands/Public/balance.js
@@ -1,17 +1,15 @@
 module.exports = async(client, msg, suffix) => {
-	let userID = msg.mentions.users.first() ? msg.mentions.users.first().id : suffix;
-	let user = await client.users.fetch(userID)
-		.catch(_ => null);
-
-	let account = user ? user.bot ? { balance: "Infinity", vip: "Infinity" } : null : null;
-
 	if (!msg.author.support || !suffix) {
-		if (!account) account = await msg.author.account;
+		let account = await msg.author.account;
 
 		msg.channel.send({
 			embed: {
 				color: config.colors.info,
 				title: "Current Account Status",
+				author: {
+					name: msg.author.tag,
+					icon_url: msg.author.displayAvatarURL(),
+				},
 				fields: [{
 					name: "Your Balance",
 					value: `¥${client.format(account.balance)}`,
@@ -29,6 +27,11 @@ module.exports = async(client, msg, suffix) => {
 			},
 		});
 	} else {
+		let userID = msg.mentions.users.first() ? msg.mentions.users.first().id : suffix;
+		let user = await client.users.fetch(userID)
+			.catch(_ => null);
+
+		let account = user ? user.bot ? { balance: "Infinity", vip: "Infinity" } : null : null;
 		if (!user) return msg.channel.send({ embed: { color: config.colors.error, title: "Unknown user", description: "Couldn't find that number." } });
 		if (!account) account = await r.table("Accounts").get(userID);
 		if (!account) return msg.channel.send({ embed: { color: config.colors.error, title: "Unknown account", description: "This user doesn't have an account." } });
@@ -37,6 +40,10 @@ module.exports = async(client, msg, suffix) => {
 			embed: {
 				color: config.colors.info,
 				title: `Information for ${user.tag}`,
+				author: {
+					name: user.tag,
+					icon_url: user.displayAvatarURL(),
+				},
 				fields: [{
 					name: "Their Balance",
 					value: `¥${client.format(account.balance)}`,
