48e00670789e952427436ef02eab8734c6f61859
Mitchell3514 <10900096+Mitchell3514@users.noreply.github.com>
2019-09-22 21:56:46 +0200

    DBots

--- Events/guildCreate.js
+++ Events/guildCreate.js
@@ -1,3 +1,17 @@
+const { post } = require("snekfetch");
+const auth = require("../Configuration/auth.js");
+
 module.exports = async guild => {
-	client.log(`📥 Joined guild \`${guild.id}\`(${guild.name}). Currently in ${client.guilds.size} servers on shard ${client.shard.id}`);
+	let name = guild.name.replace(/(\*|`|_|~)/, "\\$1").replace(/discord\.(gg|io|me|li)\/([\w\d])+/g, "**Invite Link Censored**").replace(/@(everyone|here)/g, "@\u200b$1");
+	client.log(`📥 Joined guild \`${guild.id}\`(${name}). Currently in ${client.guilds.size} servers on shard ${client.shard.id}`);
+
+	// let result = await post(`https://discordbots.org/api/bots/${client.user.id}/stats`)
+	// 	.set("Authorization", auth.tokens.bots_org)
+	// 	.set("Content-Type", "application/json")
+	// 	.send({
+	// 		shard_id: client.shard.id,
+	// 		shard_count: client.shard.count,
+	// 		server_count: client.guilds.size,
+	// 	})
+	// 	.catch(e => winston.error(`Couldn't post to DBots: ${e}`));
 };
