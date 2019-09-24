bed82633084ad69e2b20e435bcc3f9d4f57a4ac7
Austin Huang <im@austinhuang.me>
2019-09-23 20:07:21 -0400

    Update 233.js

--- Internals/233.js
+++ Internals/233.js
@@ -50,7 +50,7 @@ module.exports = async(msg, myNumber) => {
 	msg.author.busy = false;
 	omsg.delete().catch(e => null);
 	if (!collected.first()) return;
-	if (collected && msg.channel.type === "text" && msg.guild.me.hasPermission("MANAGE_MESSAGES")) collected.first().delete().catch(e => null);
+	if (collected.first() && msg.channel.type === "text" && msg.guild.me.hasPermission("MANAGE_MESSAGES")) collected.first().delete().catch(e => null);
 	if (/^0$/.test(collected.first().content)) return msg.channel.send({embed:{color: config.colors.receipt, title: "Process terminated", description: "Your account has not been charged."}});
 
 	// check for same balance
