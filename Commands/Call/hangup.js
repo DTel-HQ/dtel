dec28870332caad840785cd42a528d7029966999
Mitchell3514 <10900096+Mitchell3514@users.noreply.github.com>
2019-09-30 21:15:21 +0200

    Hiding more information with vip

--- Commands/Call/hangup.js
+++ Commands/Call/hangup.js
@@ -13,10 +13,13 @@ module.exports = async(client, msg, suffix, call) => {
 		time = client.time(s, m, h);
 	}
 
+	let side = msg.channel.id === call.from.channel ? call.from : call.to;
+	let hidden = side.hidden;
+
 	// Send the stuff
 	msg.channel.send({ embed: { color: config.colors.error, title: "The call has ended!", description: `You have ended the call${call.pickedUp ? ` after ${time}` : ""}.`, footer: { text: call.id } } });
 	await r.table("Calls").get(call.id).delete();
 	await client.apiSend({ embed: { color: config.colors.error, title: "The call has ended!", description: `The other side ended the call${call.pickedUp ? ` after ${time}` : ""}.`, footer: { text: call.id } } }, msg.channel.id === call.from.channel ? call.to.channel : call.from.channel);
 	await r.table("OldCalls").insert(call);
-	client.log(`:negative_squared_cross_mark: ${call.rcall ? "rcall" : "Call"} \`${call.from.hidden ? "hidden" : call.from.channel} → ${call.to.hidden ? "hidden" : call.to.channel}\` was hung up by ${msg.author.tag} (${msg.author.id}) on the ${msg.channel.id === call.from.channel ? "from" : "to"} side. ${call.id}`);
+	client.log(`:negative_squared_cross_mark: ${call.rcall ? "rcall" : "Call"} \`${call.from.hidden ? "hidden" : call.from.channel} → ${call.to.hidden ? "hidden" : call.to.channel}\` was hung up by ${hidden ? "Anonymous" : msg.author.tag} (${hidden ? "hidden" : msg.author.id}) on the ${msg.channel.id === call.from.channel ? "from" : "to"} side. ${call.id}`);
 };
