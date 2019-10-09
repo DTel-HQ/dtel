b08161a80082a657e8751b7d1bb539d25ec2264c
Mitchell3514 <10900096+Mitchell3514@users.noreply.github.com>
2019-10-10 00:02:48 +0200

    call: get channel before other checks

--- Commands/Public/call.js
+++ Commands/Public/call.js
@@ -37,11 +37,6 @@ module.exports = async(client, msg, suffix, rcall) => {
 		.default(null);
 
 	if (!toDialDoc) return msg.channel.send({ embed: { color: config.colors.error, title: "Dialing error", description: `The requested number (${toDial.replace("*", "")}) does not exist. Call \`*411\` to check numbers.${rcall ? " Please call *611 so we can remove this number from the phonebook." : ""}` } });
-	if (new Date(toDialDoc.expiresAt).getTime() < Date.now()) return msg.channel.send({ embed: { color: config.colors.error, title: "Dialing error", description: "The number you are trying to reach has expired. Please ask them to renew it." } });
-	if (myNumber.blocked && myNumber.blocked.includes(toDialDoc.id)) return msg.channel.send({ embed: { color: config.colors.error, title: "Dialing error", description: "You have blocked this number." } });
-	if (toDialDoc.blocked && toDialDoc.blocked.includes(myNumber.id)) return msg.channel.send({ embed: { color: config.colors.error, title: "Dialing error", description: "That number can't be reached." } });
-	if (new Date(toDialDoc.expiry).getTime() < Date.now() && myNumber.id != config.aliasNumbers["*611"]) return msg.channel.send({ embed: { color: config.colors.error, title: "Billing error", description: "The number you are trying to reach has expired. Please contact the owner to renew it." } });
-
 	try {
 		await client.api.channels(toDialDoc.channel).get();
 	} catch (_) {
@@ -50,6 +45,10 @@ module.exports = async(client, msg, suffix, rcall) => {
 		if (rcall) return (await reload("./Commands/Public/rcall.js"))(client, msg);
 		else return;
 	}
+	if (new Date(toDialDoc.expiresAt).getTime() < Date.now()) return msg.channel.send({ embed: { color: config.colors.error, title: "Dialing error", description: "The number you are trying to reach has expired. Please ask them to renew it." } });
+	if (myNumber.blocked && myNumber.blocked.includes(toDialDoc.id)) return msg.channel.send({ embed: { color: config.colors.error, title: "Dialing error", description: "You have blocked this number." } });
+	if (toDialDoc.blocked && toDialDoc.blocked.includes(myNumber.id)) return msg.channel.send({ embed: { color: config.colors.error, title: "Dialing error", description: "That number can't be reached." } });
+	if (new Date(toDialDoc.expiry).getTime() < Date.now() && myNumber.id != config.aliasNumbers["*611"]) return msg.channel.send({ embed: { color: config.colors.error, title: "Billing error", description: "The number you are trying to reach has expired. Please contact the owner to renew it." } });
 
 	let activeCall = await r.table("Calls").getAll(toDial, { index: "fromChannel" }).nth(0).default(null);
 	if (!activeCall) activeCall = await r.table("Calls").getAll(toDial, { index: "toChannel" }).nth(0).default(null);
