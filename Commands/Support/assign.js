c5a34be43bbc4b1ce675db8c8b17997dc1f4500a
Mitchell <contact@mitchells.work>
2019-11-05 14:30:00 +0100

    [aesthetic] assign - counting to 11 is hard
    It will now tell you to count to 11 before blaming your channel input

--- Commands/Support/assign.js
+++ Commands/Support/assign.js
@@ -7,7 +7,7 @@ module.exports = async(client, msg, suffix) => {
 		// Genuine offbrand fix
 		number = suffix.substring(suffix.indexOf(`${msg.mentions.channels.first().toString()}`) + msg.mentions.channels.first().toString().length + 1).trim();
 	// otherwise if they used an id
-	} else if (/^0(900|30\d|8(00|44))\d{7}$/.test(await client.replaceNumber(suffix.split(" ")[0]))) {
+	} else if (/\d{7,13}$/.test(await client.replaceNumber(suffix.split(" ")[0]))) {
 		channelID = suffix.split(" ")[1];
 		number = await client.replaceNumber(suffix.split(" ")[0]);
 	} else {
@@ -15,17 +15,13 @@ module.exports = async(client, msg, suffix) => {
 		number = await client.replaceNumber(suffix.split(" ")[1]);
 	}
 
+	if (!/^0(900|30\d|8(00|44))\d{7}$/.test(number)) return msg.channel.send({ embed: { color: config.colors.error, title: "Invalid number", description: "**Is this a valid 11-digit number?** Course not, you dumbass" } });
 	let channel = await client.channels.get(channelID);
 	if (!channel) return msg.channel.send({ embed: { color: config.colors.error, title: "Invalid channel", description: "Couldn't find that channel." } });
 
 	number = client.replaceNumber(number);
 
-	if (!/^0(900|30\d|8(00|44))\d{7}$/.test(number)) {
-		return msg.channel.send({ embed: { color: config.colors.error, title: "Invalid number", description: "**Is this a valid 11-digit number?** Course not, you dumbass" } });
-	}
-	if (channel.type == "dm" && !/^0(900|8(00|44))\d{7}$/.test(number)) {
-		return msg.channel.send({ embed: { color: config.colors.error, title: "Invalid prefix", description: "**Don't you know what prefix a dm channel has?** It's `0900`." } });
-	}
+	if (channel.type == "dm" && !/^0(900|8(00|44))\d{7}$/.test(number)) return msg.channel.send({ embed: { color: config.colors.error, title: "Invalid prefix", description: "**Don't you know what prefix a dm channel has?** It's `0900`." } });
 
 	let foundNumber;
 	foundNumber = await r.table("Numbers").get(number).default(null);
