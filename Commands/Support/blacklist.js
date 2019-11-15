b50839dc7aac7cf66722545d9bc81a427a3e9956
Mitchell <contact@mitchells.work>
2019-11-15 21:48:16 +0100

    [Fix] Blacklist.js - guilds

--- Commands/Support/blacklist.js
+++ Commands/Support/blacklist.js
@@ -5,24 +5,12 @@ module.exports = async(client, msg, suffix) => {
 	if (msg.mentions.users.first()) suffix = (await msg.mentions.users.first()).id;
 
 	let user = await client.users.fetch(suffix).catch(e => null);
-	let guild, shard;
-	if (!user) {
-		let results = await client.shard.broadcastEval(`this.guilds.resolve("${suffix}")`);
-		for (let result of results) {
-			if (result) {
-				guild = result;
-				shard = results.indexOf(result);
-				break;
-			}
-		}
-	}
-
+	if (!user) let guild = await client.guilds.resolve(suffix);
 	if (!user && !guild) return msg.channel.send({ embed: { color: config.colors.error, title: "Unknown ID", description: "Couldn't find a user or guild with that ID" } });
-
 
 	let blacklisted = user ? user.blacklisted : guild.blacklisted;
 	if (blacklisted) {
-		let res = user ? user.unBlacklist() : await client.shard.broadcastEval(`if (this.shard.id=== ${shard}) this.guilds.resolve("${suffix}").unBlacklist()`);
+		let res = user ? user.unBlacklist() : await guild.unBlacklist();
 		client.log(`:wrench: ID \`${suffix}\` has been removed from the blacklist by ${msg.author.tag}.`);
 		return msg.channel.send({ embed: {
 			color: config.colors.success,
@@ -43,7 +31,7 @@ module.exports = async(client, msg, suffix) => {
 	if (suffix === msg.author.id) return msg.channel.send({ embed: { color: config.colors.error, title: "Dumbo", description: "You dumb :b:oi, don't blacklist yourself!" } });
 	if (user && user.support) return msg.channel.send({ embed: { color: config.colors.error, title: "Staff", description: "Don't like your collegue? Think of a better way to get rid of them!" } });
 
-	let res = user ? user.blacklist() : await client.shard.broadcastEval(`if (this.shard.id === ${shard}) this.guilds.resolve("${suffix}").blacklist()`);
+	let res = user ? user.blacklist() : guild.blacklist();
 	client.log(`:hammer: ID \`${suffix}\` has been added to the blacklist by ${msg.author.tag}.`);
 	msg.channel.send({ embed: {
 		color: config.colors.success,
