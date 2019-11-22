354f02a1efcd41a19479d7c87db6b7a66fe08574
Ed Luff <beartechtalks@gmail.com>
2019-11-22 10:22:09 +0000

    [config] use tabs

--- Configuration/aliases.js
+++ Configuration/aliases.js
@@ -1,21 +1,21 @@
 /* eslint-disable no-inline-comments */
 module.exports = {
-        // Command aliases
-        aliasCommands: {
-                bal: "balance",
-                bc: "broadcast",
-                bl: "blacklist",
-                cmds: "help",
-                commands: "help",
-                contact: "contacts",
-                credits: "balance",
-                delstrike: "rmstrike",	
-                id: "identify",
-                link: "links",
-                mentions: "mention",
-                msg: "message",
-                perm: "permcheck",
-                promo: "promote",
-                wl: "whitelist",
-       },
+	// Command aliases
+	aliasCommands: {
+		bal: "balance",
+		bc: "broadcast",
+		bl: "blacklist",
+		cmds: "help",
+		commands: "help",
+		contact: "contacts",
+		credits: "balance",
+		delstrike: "rmstrike",	
+		id: "identify",
+		link: "links",
+		mentions: "mention",
+		msg: "message",
+		perm: "permcheck",
+		promo: "promote",
+		wl: "whitelist",
+	},
 };
