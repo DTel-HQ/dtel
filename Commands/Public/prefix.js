04f0b260dc1e7a283b23f20bc4df7be930f150c8
Mitchell3514 <10900096+Mitchell3514@users.noreply.github.com>
2019-11-25 20:17:17 +0100

    [Bug] prefix - undefined prefix and not deleting prefix

--- Commands/Public/prefix.js
+++ Commands/Public/prefix.js
@@ -1,19 +1,16 @@
 module.exports = async(client, msg, suffix) => {
-	if (!/^\S{1,4}$/.test(suffix)) return msg.channel.send({ embed: { color: config.colors.info, title: "Command usage", description: "Usage: `>prefix [prefix]`\nNote that prefixes are per-user and have to be between 1 and 4 characters.\n", footer: { text: `Your current prefix: ${msg.author.account.prefix} - To remove your prefix: >prefix ${config.prefix}` } } });
-
-	// to get rid of prefix
-	if (suffix === config.prefix) suffix = null;
+	if (!/^\S{1,4}$/.test(suffix)) return msg.channel.send({ embed: { color: config.colors.info, title: "Command usage", description: `Usage: \`>prefix [prefix]\`\nNote that prefixes are per-user and have to be between 1 and 4 characters.${msg.author.prefix ? `\nYour current prefix: \`msg.author.prefix}\`` : ""}`, footer: { text: `To remove your prefix: >prefix ${config.prefix}` } } });
 
 	// get / make account
 	let account = await msg.author.account();
 
-	if (suffix) {
-		await r.table("Accounts").get(msg.author.id).update({ prefix: suffix });
+	if (suffix != config.prefix) {
+		await r.table("Accounts").get(account.id).update({ prefix: suffix });
 	} else {
 		delete account.prefix;
-		await r.table("Accounts").get(msg.author.id).replace(account);
+		await r.table("Accounts").get(account.id).replace(account);
 	}
 
-	if (suffix) return msg.channel.send({ embed: { color: config.colors.success, title: "Success", description: `Your custom (user) prefix is now \`${suffix}\`. You can still use \`${config.prefix}\`.`, footer: { text: `To get rid of your prefix, simply do >prefix ${config.prefix}` } } });
+	if (suffix != config.prefix) return msg.channel.send({ embed: { color: config.colors.success, title: "Success", description: `Your custom (user) prefix is now \`${suffix}\`. You can still use \`${config.prefix}\`.`, footer: { text: `To get rid of your prefix, simply do >prefix ${config.prefix}` } } });
 	else return msg.channel.send({ embed: { color: config.colors.success, title: "Success", description: "Your custom (user) prefix has been removed." } });
 };
