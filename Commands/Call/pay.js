940012e6f489e248924c2d21e46abb7b961ac402
Ed Luff <beartechtalks@gmail.com>
2019-12-02 19:45:42 +0000

    Update pay.js

--- Commands/Call/pay.js
+++ Commands/Call/pay.js
@@ -1,1 +1,1 @@
-module.exports = (client, msg, suffix) => require(".../Public/Pay.js")(client, msg, suffix);
+module.exports = (client, msg, suffix) => require(".../Public/pay.js")(client, msg, suffix);
