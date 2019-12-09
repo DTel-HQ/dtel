5046e0251efa9bb8458e9273d627950b9e696597
Mitchell3514 <10900096+Mitchell3514@users.noreply.github.com>
2019-12-09 17:39:21 +0100

    [UX] better ping

--- Commands/Public/ping.js
+++ Commands/Public/ping.js
@@ -1,3 +1,9 @@
 module.exports = async(client, msg, suffix) => {
-	msg.channel.send(`pong - ${client.ws.ping}`);
+	let latency = client.ws.ping;
+	let omsg = await msg.channel.send("Measuring...");
+	let ping = Math.round(omsg.createdTimestamp - msg.createdTimestamp);
+	let embed = { color: config.colors.info, title: "Pong", description: `API Latency: ${latency}\nMeasured time: ${ping}}` };
+	omsg.edit({ content: "", embed: embed }).catch(_ => {
+		msg.channel.send({ embed: embed });
+	});
 };
