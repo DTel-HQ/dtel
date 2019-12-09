ca7ad9f0fce2342849427b27791557454af7dd45
Mitchell3514 <10900096+Mitchell3514@users.noreply.github.com>
2019-12-09 17:41:05 +0100

    [UX] ping

--- Commands/Public/ping.js
+++ Commands/Public/ping.js
@@ -1,8 +1,8 @@
 module.exports = async(client, msg, suffix) => {
-	let latency = client.ws.ping;
-	let omsg = await msg.channel.send("Measuring...");
-	let ping = Math.round(omsg.createdTimestamp - msg.createdTimestamp);
-	let embed = { color: config.colors.info, title: "Pong", description: `API Latency: ${latency}\nMeasured time: ${ping}}` };
+	const latency = Math.round(client.ws.ping);
+	const omsg = await msg.channel.send("Measuring...");
+	const ping = Math.round(omsg.createdTimestamp - msg.createdTimestamp);
+	const embed = { color: config.colors.info, title: "Pong", description: `API Latency: ${latency}ms\nMeasured time: ${ping}ms` };
 	omsg.edit({ content: "", embed: embed }).catch(_ => {
 		msg.channel.send({ embed: embed });
 	});
