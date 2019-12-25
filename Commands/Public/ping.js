8beff8c15c4ca1b65f944cc9a70c9310fa2b54fa
Mitchell <contact@mitchells.work>
2019-12-25 20:03:16 +0100

    Uselessly making ping.js better

--- Commands/Public/ping.js
+++ Commands/Public/ping.js
@@ -1,6 +1,6 @@
 module.exports = async(client, msg, suffix) => {
-	const latency = Math.round(client.ws.ping);
 	const omsg = await msg.channel.send("Measuring...");
+	const latency = Math.round(client.ws.ping);
 	const ping = Math.round(omsg.createdTimestamp - msg.createdTimestamp);
 	const embed = { color: config.colors.info, title: "Pong", description: `API Latency: ${latency}ms\nMeasured time: ${ping}ms` };
 	omsg.edit({ content: "", embed: embed }).catch(_ => {
