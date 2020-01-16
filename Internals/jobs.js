f7f2aff212f4de746dbd27b5472e0580aa30de24
Austin Huang <im@austinhuang.me>
2020-01-15 20:16:57 -0500

    [UX] automatic renewal?

--- Internals/jobs.js
+++ Internals/jobs.js
@@ -259,16 +259,14 @@ scheduleJob("0 0 0 * * *", async() => {
 		// v2 number has expiry as a date object, yet v3 has string (timestamp) so compromise
 		let expiryMS = typeof number.expiry === "object" ? number.expiry.getTime() : new Date(number.expiry).getTime();
 
-		// First check wether number isn't (>15d) expired - then warn/delete.
 		if (expiryMS < deleteMS) {
 			await client.delete(number, { force: true, log: true, origin: "scheduled_expired" });
-
 			otitle = `Your number has been deleted`;
 			odesc = `Your number (${number.id}) in <#${channel.id}> has been deleted as it has been expired for >${deleteDays} days.`;
 			ctitle = `This channel's number has been deleted`;
 			cdesc = `This channel's number (${number.id}) has been deleted as it has been expired for >${deleteDays} days.`;
 		} else if (expiryMS < warnMS && expiryMS > warnMS - 86400000) {
-			otitle = `Your number soon be deleted.`;
+			otitle = `Your number will soon be deleted`;
 			odesc = `Your number ${number.id} in <#${channel.id}> has expired and will be automatically deleted in ${deleteDays - warnDays} days. To prevent losing your number (and all that comes with it), please extend the duration of your number by calling \`*233\`. `;
 			ctitle = `This number will soon be deleted`;
 			cdesc = `This channel's number (${number.id}) has been expired for >${warnDays} days and will automatically be deleted in ${deleteDays - warnDays}. To prevent losing this number and all its settings, please extend it by calling \`*233\`.`;
@@ -277,8 +275,27 @@ scheduleJob("0 0 0 * * *", async() => {
 			odesc = `Your number ${number.id} in <#${channel.id}> has been expired for >${lastWarn} days and will automatically be deleted in **24h**. To prevent losing your number (and all that comes with it), please extend the duration of your number by calling \`*233\`. `;
 			ctitle = `❕This number will be deleted in 24h❕`;
 			cdesc = `This channel's number (${number.id}) has been expired for >${lastWarn} days and will automatically be deleted in **24h**. To prevent losing this number and all its settings, please extend it by calling \`*233\`.`;
+		} else if (expiryMS > time && expiryMS < time + 86400000) {
+			let account = await (await client.users.fetch(owner)).account();
+			let newBalance = account.balance - config.renewalRate;
+			if (newBalance >= 0) {
+				let newExpiry = new Date(number.expiry);
+				newExpiry.setMonth(newExpiry.getMonth() + 1);
+				await r.table("Accounts").get(account.id).update({ balance: newBalance });
+				await r.table("Numbers").get(myNumber.id).update({ expiry: newExpiry });
+
+				otitle = `Automatic Renewal`
+				odesc = `Your number ${number.id} in <#${channel.id}> has expired. Seeing you have a sufficient balance, we have renewed it for you!\n\n**${config.renewalRate} credits have been deducted from your account.** Your current balance is ${newBalance} credits.`;
+				ctitle = `Automatic Renewal`;
+				cdesc = `This channel's number (${number.id}) has expired. Seeing the owner has a sufficient balance, we have renewed it for you!`;
+			}
+			else {
+				otitle = `Automatic Renewal Failure`
+				odesc = `Your number (${number.id}) in <#${channel.id}> has expired. I am unable to deduct money from your account, so please call \`*233\` from <#${channel.id}> to manually renew your number.`;
+				ctitle = `Automatic Renewal Failure`;
+				cdesc = `This channel's number (${number.id}) has expired. I am unable to deduct money from the owner's account, so please call \`*233\` to manually renew the number.`;
+			}
 		}
-		else continue;
 
 		embed.setTitle(ctitle)
 			.setDescription(cdesc);
