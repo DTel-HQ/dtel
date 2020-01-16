15d2c9ffb94f867595a66fd60df8858279312a3f
Mitchell3514 <10900096+Mitchell3514@users.noreply.github.com>
2020-01-16 21:03:17 +0100

    [Feature] auto-remove left members from mention.

---
+++ Events/guildMemberRemove.js
@@ -0,0 +1,16 @@
+module.exports = async member => {
+	const guild = member.guild;
+	const numbers = r.table("Numbers").getAll(guild.id, { index: "guild" }).default([]);
+
+	if (!numbers.length) return;
+
+	let n, p;
+	for (let i = 0; i < numbers.length; i++) {
+		n = numbers[i];
+		if (!n.mentions) continue;
+		p = n.mentions.indexOf(`<@${member.id}>`);
+		if (p < 0) continue;
+		n.mentions.splice(p, 1);
+		r.table("Numbers").get(n.id).update({ mentions: n.mentions });
+	}
+};
