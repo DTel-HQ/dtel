699e5519b76a1413142548adc197b29844c7283f
Austin Huang <im@austinhuang.me>
2020-01-19 21:53:04 -0500

    [Enhancement] 2 digit

--- Commands/Public/convert.js
+++ Commands/Public/convert.js
@@ -24,7 +24,7 @@ module.exports = async(client, msg, suffix) => {
 			return msg.channel.send({ embed: { color: config.colors.error, title: "API error", description: `\`\`\`\n${err}\n\`\`\`` } });
 		}
 	}
-	amount = parseInt(amount);
+	amount = parseFloat(amount).toFixed(2);
 	currency = currency.toUpperCase();
 
 	let account = await msg.author.account();
@@ -36,7 +36,7 @@ module.exports = async(client, msg, suffix) => {
 	try {
 		newTransaction = await DClient.transactions.create({
 			to: currency,
-			amount: parseInt(amount),
+			amount: amount,
 			user: msg.author.id,
 		});
 	} catch (err) {
