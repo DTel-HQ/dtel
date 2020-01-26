df36f707edefc5078c04a071c2192622e8eeb4a1
Mitchell <contact@mitchells.work>
2020-01-26 20:06:06 +0100

    Auto leave guilds that are blacklisted.

--- Events/guildCreate.js
+++ Events/guildCreate.js
@@ -1,6 +1,8 @@
 const auth = require("../Configuration/auth.js");
 
 module.exports = async guild => {
+	let blacklisted = r.table("Blacklist").get(guild.id);
+	if (blacklisted) return guild.leave();
 	let name = guild.name.replace(/(\*|`|_|~)/, "\\$1").replace(/discord\.(gg|io|me|li)\/([\w\d])+/g, "**Invite Link Censored**").replace(/@(everyone|here)/g, "@\u200b$1");
 	client.log(`📥 Joined guild \`${guild.id}\`(${name}). Currently in ${client.guilds.size} servers on cluster ${client.shard.id}`);
 };
