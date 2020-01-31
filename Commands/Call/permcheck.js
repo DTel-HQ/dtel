028f5f90f23381aaae17263626984f0745b182ef
Mitchell <contact@mitchells.work>
2020-01-31 22:12:03 +0100

    [Fix] Call/permcheck - DM numbers

--- Commands/Call/permcheck.js
+++ Commands/Call/permcheck.js
@@ -5,6 +5,7 @@ module.exports = async(client, msg, suffix, call) => {
 
 	let channel = await client.channels.get(call.to.channel === config.supportChannel ? call.from.channel : call.to.channel);
 	if (!channel) return msg.channel.send({ embed: { color: config.colors.error, title: "Unknown channel", description: "Couldn't find the other side." } });
+	if (channel.type === "dm") return msg.channel.send({ embed: { color: config.colors.info, title: "0900...", description: "Of course they'll have permission..." } });
 	let guildID = channel.guild.id;
 	require("../Support/permcheck.js")(client, msg, suffix, guildID);
 };
