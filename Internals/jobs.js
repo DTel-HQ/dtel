7c746831280bdbc5e6ed2cd8b0054ce5b7ea59b6
Mitchell3514 <10900096+Mitchell3514@users.noreply.github.com>
2020-02-03 00:20:22 +0100

    [UX] gazette - adding % change

--- Internals/jobs.js
+++ Internals/jobs.js
@@ -336,11 +336,26 @@ scheduleJob("0 0 0 * * *", async() => {
 scheduleJob("0 0 * * * *", async() => {
 	const currencies = await Discoin.currencies.getMany("filter=name||$excl||Test&sort=id,ASC"),
 		emojis = client.guilds.get("347859709711089674").emojis;
+
 	const prevrates = (await r.table("Accounts").get("discoin")).rates;
+	const strings = {};
+	for (let currency of currencies) {
+		if (currency.value === prevrates[currency.id]) {
+			strings[currency.id] = "";
+			continue;
+		}
+		let change = currency.value / prevrates[currency.id];
+		if (change < 1) change = 1 - change;
+		else change -= 1;
+		change = Math.round(change * 10000) / 100;
+		if (currency.value > prevrates[currency.id]) strings[currency.id] = ` :chart_with_upwards_trend: +${change}%`;
+		else strings[currency.id] = ` :chart_with_downwards_trend: -${change}%`;
+	}
+
 	await client.apiSend({ embed: {
 		title: "Discoin Rates",
 		color: 0x2196f3,
-		description: currencies.map(c => `${emojis.find(e => e.name === c.id).toString()} ${c.value}${prevrates[c.id] ? prevrates[c.id] < c.value ? " :chart_with_upwards_trend:" : prevrates[c.id] > c.value ? " :chart_with_downwards_trend:" : "" : ""}`),
+		description: currencies.map(c => `${emojis.find(e => e.name === c.id).toString()} ${c.value}${strings[c.id]}`).join("\n"),
 		timestamp: new Date(),
 	} }, "661239975752499231");
 	const newrates = {};
