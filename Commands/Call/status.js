0891cf45d7c8707c45e233c995bb61544614b0a9
Mitchell3514 <10900096+Mitchell3514@users.noreply.github.com>
2020-02-08 18:25:24 +0100

    Feat(commands) - status.js
    Adds status, request from user

---
+++ Commands/Call/status.js
@@ -0,0 +1,18 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = (client, msg, suffix, call) => {
+	if (!call.pickedUp) return;
+
+	const startDate = new Date(call.startedAt);
+	const time = Date.now() - startDate.getTime();
+	const messages = call.messages.length;
+
+	const embed = new MessageEmbed()
+		.setColor(config.colors.info)
+		.setTitle("Call status")
+		.addField("Time elapsed", client.time(time), true)
+		.addField("Message count", messages, true)
+		.setFooter(call.id)
+		.setTimestamp();
+	msg.channel.send({ embed: embed });
+};
