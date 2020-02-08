935353710c745ac476fa826d2623624a0c5bb532
Mitchell3514 <10900096+Mitchell3514@users.noreply.github.com>
2020-02-08 18:35:41 +0100

    Fix(Commands/status) - correct time
    TS would've fixed this

--- Commands/Call/status.js
+++ Commands/Call/status.js
@@ -4,13 +4,21 @@ module.exports = (client, msg, suffix, call) => {
 	if (!call.pickedUp) return;
 
 	const startDate = new Date(call.startedAt);
-	const time = Date.now() - startDate.getTime();
+	let time = (Date.now() - startDate.getTime()) / 1000;
+	const days = Math.floor(time / (24 * 60 * 60));
+	time -= days * (24 * 60 * 60);
+	const hours = Math.floor(time / (60 * 60));
+	time -= hours * (60 * 60);
+	const minutes = Math.floor(time / 60);
+	time -= minutes * 60;
+	const seconds = time;
+
 	const messages = call.messages ? call.messages.length : 0;
 
 	const embed = new MessageEmbed()
 		.setColor(config.colors.info)
 		.setTitle("Call status")
-		.addField("Time elapsed", client.time(time), true)
+		.addField("Time elapsed", client.time(seconds, minutes, hours, days), true)
 		.addField("Message count", messages, true)
 		.setFooter(call.id)
 		.setTimestamp();
