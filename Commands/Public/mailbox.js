05ced530767d9f7b8d152419af8b596e85532728
Austin Huang <im@austinhuang.me>
2020-02-12 16:58:47 -0500

    Update mailbox.js

--- Commands/Public/mailbox.js
+++ Commands/Public/mailbox.js
@@ -280,7 +280,7 @@ module.exports = async(client, msg, suffix) => {
 				case "0": {
 					msg.author.busy = false;
 					omsg.delete().catch(e => 
-						omsg.channel.send({ embed: { color: config.colors.info, title: "You closed the p'tit door of your mailbox...", description: "...and locked it with your key.\nRemember, you can always open it again with `>mailbox`!", footer: { text: msg.author.id, icon_url: msg.author.displayAvatarURL() } } });
+						omsg.channel.send({ embed: { color: config.colors.info, title: "You closed the p'tit door of your mailbox...", description: "...and locked it with your key.\nRemember, you can always open it again with `>mailbox`!", footer: { text: msg.author.id, icon_url: msg.author.displayAvatarURL() } } })
 					);
 					break;
 				}
