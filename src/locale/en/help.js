627b6e36843241e48e1943776b5f2b8de5e26117
Mitchell3514 <10900096+Mitchell3514@users.noreply.github.com>
2020-02-19 21:28:58 +0100

    refactor(translations) json format

--- src/locale/en/help.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	title: "List of commands",
-	description: "For more information, use `info`, `links` or call Customer support: `>dial *611`",
-	fields: [
-		{ name: "Get yourself a number to call others!", value: "Do `>wizard` and follow the prompts. Once you got one, you can..." },
-		{ name: ">dial / >call", value: "Dial a specific DiscordTel number you have in mind. Or..." },
-	],
-};
--- src/locale/en/sample.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	string: "This is a string",
-	string_content: "This is a string with {{context}}",
-	addcredit: "{{amount}} has been added to your balance",
-	addcredit_user: "{{amount}} has been added to your balance by {{user}}",
-};
