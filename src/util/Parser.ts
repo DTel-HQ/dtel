48f5e8177f61806be605ae04f42e8344a3d4cff3
Mitchell3514 <10900096+Mitchell3514@users.noreply.github.com>
2020-02-22 23:05:27 +0100

    docs(parser): add doc comments

--- src/util/Parser.ts
+++ src/util/Parser.ts
@@ -11,6 +11,13 @@ export default class Parser {
 	constructor (protected client: Client) {
 	}
 
+	/**
+	* Check type of suffix content
+	* @param s: string
+	* @returns { type: number, ID: string? }
+	* @public @static
+	*/
+
 	// Export the 'type' of suffix content and 'id' for mentions
 	public static type(s: string): suffixType {
 		if (!s || typeof s != "string") throw new Error("Parser(type): Suffix must be a defined string");
@@ -22,11 +29,28 @@ export default class Parser {
 		return { type: SuffixTypes["text"], id: null };
 	}
 
-	public object(id: string, type: number): any {
+	/**
+	* @todo will probably be an import from different util later on
+	* returns an instance resolved from type and ID
+	* @param id: string
+	* @param type: number
+	* @returns instance
+	* @public
+	*/
+
+	public instance(id: string, type: number): any {
 		if (type === SuffixTypes["DTelNumber"]) return ; // TODO: return number
 		else if (type === SuffixTypes["userMention"]) return this.client.users.resolve(id);
 		else if (type === SuffixTypes["channelMention"]) return this.client.channels.resolve(id);
+		return null;
 	}
+
+	/**
+	* Returns an array with suffix contents
+	* @param suffix: string
+	* @returns { content: content, type: typeNumber, id: ID?, instance: instance? }[]
+	* @public
+	*/
 
 	public parseSuffix(suffix: string): object[] {
 		const parsed: object[] = [];
@@ -34,23 +58,22 @@ export default class Parser {
 
 		let toParse: string,
 			suffixType: suffixType,
-			suffixObject: object,
 			result: object = {};
 
 		for (let step = 0; step < suffixes.length; step++) {
 			toParse = suffixes[step];
+			result = { ...result, content: toParse };
 			suffixType = Parser.type(toParse);
 			result = { ...result, ...suffixType };
 
 			if (suffixType.type === SuffixTypes["text"]) {
 				let text = suffix.split(" ").splice(step).join(" ");
-				result = { ...result, text: text }
+				result = { ...result, content: text }
 			}
 
 			if (suffixType.id && suffixType.type) {
-				this.object(suffixType.id, suffixType.type).then((object: any) => {
-					suffixObject = object;
-					result = { ...result, ...suffixObject };
+				this.instance(suffixType.id, suffixType.type).then((instance: any) => {
+					result = { ...result, instance: instance };
 				});
 			}
 
