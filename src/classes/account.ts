c86c04e190875aae66bda159f7750084f944b2f2
Mitchell3514 <10900096+Mitchell3514@users.noreply.github.com>
2020-02-23 23:59:21 +0100

    feat(account): language, prefix, util

--- src/classes/account.ts
+++ src/classes/account.ts
@@ -1,5 +1,7 @@
 import DMNumber from "./dmnumber";
-import { ReqlClient } from "rethinkdbdash"
+import { util } from "../constants/interfaces";
+import { Languages } from "../constants/enums";
+import { settings } from "../configuration/config";
 
 /**
 * Create an account
@@ -12,13 +14,13 @@ import { ReqlClient } from "rethinkdbdash"
 export default class Account {
 	private _balance: number = 100;
 	private _frozen: boolean = false;
-	private _prefix: string = ">";
+	private _prefix: string = settings.prefix;
 	private _status: string = "idle";
-	private _language: string = "en";
+	private _language: keyof typeof Languages = "en";
 	public vip: number = 0;
 	readonly toStore: any[] = []
 
-	constructor(readonly id: string, private r: ReqlClient, account?: object) {
+	constructor(readonly id: string, private util: util, account?: object) {
 		Object.assign(this, account);
 	}
 
@@ -26,10 +28,8 @@ export default class Account {
 		return this._balance;
 	}
 
-	public freeze(status: string): boolean {
-		if (this._frozen) return false;
-		this._status = status;
-		return this._frozen = true;
+	public get language(): string {
+		return this._language;
 	}
 
 	public get number(): DMNumber | null {
@@ -40,28 +40,37 @@ export default class Account {
 		return this._prefix;
 	}
 
-	public setBalance(amount: number): Account {
-		this._balance = amount;
-		this.r!.table("Accounts").get(this.id).update({ balance: this._balance });
-		return this;
-	}
-
-	public setPrefix(prefix: string): Account {
-		this._prefix = prefix;
-		this.r!.table("Accounts").get(this.id).update({ prefix: this._prefix });
-		return this;
-	}
-
 	public get status(): string {
 		return this._status!;
 	}
 
 	public get transactions(): any {
-		return this.r.table("Transactions").filter(this.r.row("fromID").eq(this.id).or(this.r.row("toID").eq(this.id))).default([]);
+		// TODO: make it an index
+		return this.util.db.r.table("Transactions").filter(this.util.db.r.row("fromID").eq(this.id).or(this.util.db.r.row("toID").eq(this.id))).default([]);
 	}
 
-	public get language(): string {
-		return this._language;
+	public freeze(status: string): boolean {
+		if (this._frozen) return false;
+		this._status = status;
+		return this._frozen = true;
+	}
+
+	public setBalance(amount: number): Account {
+		this._balance = amount;
+		this.util.db.r!.table("Accounts").get(this.id).update({ balance: this._balance });
+		return this;
+	}
+
+	public setLanguage(lang: keyof typeof Languages) {
+		this._language = lang;
+		this.util.db.r!.table("Accounts").get(this.id).update({ language: this._language });
+	}
+
+	public setPrefix(prefix: string | null): Account {
+		if (prefix === null) this._prefix = settings.prefix;
+		else this._prefix = prefix;
+		this.util.db.r!.table("Accounts").get(this.id).update({ prefix: this._prefix });
+		return this;
 	}
 
 	public unfreeze(): Account {
