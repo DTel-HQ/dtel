c58f5dcb2316714fe888e59315c368b6c1b76ed4
Ed Luff <beartechtalks@gmail.com>
2020-02-23 22:20:05 +0000

    enums: languages
    I’m presuming this is what we’ll want to do

--- src/constants/enums.ts
+++ src/constants/enums.ts
@@ -1,3 +1,5 @@
 export enum NumberTypes {DTelNumber = -1, GuildNumber, DMNumber};
 
 export enum SuffixTypes {DTelNumber, userMention, channelMention, Number, text};
+
+export enum Languages {en, fr, de, nl};
