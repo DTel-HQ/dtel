a2fe43f945f2c1f7dcabe155516c1b6e66e62d31
SunburntRock89 <SunburntRock89@gmail.com>
2020-04-08 19:59:50 +0100

    Correct playing message

--- Internals/jobs.js
+++ Internals/jobs.js
@@ -65,7 +65,7 @@ scheduleJob("*/15 * * * * *", async() => {
 	let guildCount = (await client.shard.fetchClientValues("guilds.cache.size")).reduce((a, b) => a + b, 0);
 	let sec = new Date().getSeconds();
 	if ([14, 15, 16, 44, 45, 46].includes(sec)) {
-		let userCount = (await client.shard.broadcastEval("this.guilds.cache.reduce((prev, guild) => prev + guild.memberCount, 0)")).reduce((prev, curr) => prev + curr, 0);
+		let userCount = (await client.shard.fetchClientValues("users.cache.size")).reduce((a, b) => a + b, 0);
 		client.shard.broadcastEval(`this.user.setPresence({ activity: { name: \`${guildCount} servers and ${userCount} users | >help | [\${this.shard.id}]\`, type: 3 } });`);
 	} else {
 		let calls = (await r.table("Calls")).length;
