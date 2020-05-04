6830d0da9e46616b96a3a6310eb375f3ec78dd8f
Ed L <beartechtalks@gmail.com>
2020-05-04 15:36:00 +0100

    broadcast: add sender id

--- Commands/Support/broadcast.js
+++ Commands/Support/broadcast.js
@@ -3,6 +3,8 @@ module.exports = async(client, msg, suffix) => {
 	let input = suffix.split(" ")[0];
 	let number;
 
+	let broadcaster = message.author.id;
+
 	if (!input) return msg.channel.send({ embed: { color: config.colors.info, title: "Command usage", description: ">broadcast [number/channelID] [message]" } });
 	number = await r.table("Numbers").getAll(input, { index: "channel" }).nth(0).default(null);
 	input = client.replaceNumber(input);
@@ -14,13 +16,13 @@ module.exports = async(client, msg, suffix) => {
 	let message = suffix.split(" ");
 	message.splice(0, 1);
 	if (!message.length) return msg.channel.send({ embed: { color: config.colors.error, title: "What The Fuck", description: "That's the most useless message I've seen..." } });
-	if (message.length > 1800) return msg.channel.send({ embed: { color: config.colors.error, title: "Oi mate", description: "What ya writing an essay for? (too many characters)" } });
+	if (message.length > 1800) return msg.channel.send({ embed: { color: config.colors.error, title: "Oi mate", description: "What ya' writing an essay for? (too many characters)" } });
 
 	try {
-		await client.apiSend({ embed: { color: config.colors.info, title: "❕ Message from DTel staff ❕", description: `**${message.join(" ")}**` } }, channel);
+		await client.apiSend({ embed: { color: config.colors.info, title: "❕ Message from DTel staff ❕", description: `**${message.join(" ")}**`, footer: `From ${broadcaster}` } }, channel);
 		return msg.channel.send({ embed: { color: config.colors.success, title: "Message succesfully sent!", description: `**• Message**: ${message.join(" ")}\n**• Channel**: ${number.channel}`, author: { name: `By ${msg.author.tag}`, icon_url: msg.author.displayAvatarURL() } } });
 	} catch (err) {
 		await r.table("Numbers").get(number.id).delete();
-		return msg.channel.send({ embed: { color: config.colors.error, title: "Couldn't send a message.", description: "The number has now been deleted." } });
+		return msg.channel.send({ embed: { color: config.colors.error, title: "Couldn't send your message.", description: "The number has now been deleted." } });
 	}
 };
