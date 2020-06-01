83fe516eec0999912309fd37a7c26a5999d20753
Mitchell3514 <10900096+Mitchell3514@users.noreply.github.com>
2020-06-01 22:34:47 +0200

    refactor(pull): embed color to receipt

--- Commands/Private/pull.js
+++ Commands/Private/pull.js
@@ -2,5 +2,5 @@ const { exec } = require("child_process");
 
 module.exports = async(client, msg, suffix) => {
 	await exec("git pull");
-	return msg.channel.send({ embed: { color: config.colors.success, title: "Executed Git pull", description: "Use `>restart` if there was a change to the following files/folders: \n• ./*.js\n• ./Configuration/\n• ./Database/\n• ./Structures/\n• ./Internals/ (except for `233`, `411` and `callHandler`)" } });
+	return msg.channel.send({ embed: { color: config.colors.receipt, title: "Executed Git pull", description: "Use `>restart` if there was a change to the following files/folders: \n• ./*.js\n• ./Configuration/\n• ./Database/\n• ./Structures/\n• ./Internals/ (except for `233`, `411` and `callHandler`)" } });
 };
