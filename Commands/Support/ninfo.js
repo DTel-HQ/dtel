f56b92a341df633d2383d55fdd7fa9572d8e0c82
Mitchell3514 <10900096+Mitchell3514@users.noreply.github.com>
2020-06-09 14:44:59 +0200

    formatting ninfo

--- Commands/Support/ninfo.js
+++ Commands/Support/ninfo.js
@@ -40,10 +40,10 @@ module.exports = async(client, msg, suffix) => {
 		.addField("Channel", `ID: \`${channel.id}\`\nName: ${channel.type === 1 ? "DM Channel" : channel.name}\nDM: ${channel.type === 1 ? "True" : "False"}`, true)
 		.addField("Owner", `ID: \`${guild ? guild.owner_id : channel.recipients[0].id}\`\nTag: ${owner.tag}\nBlacklisted: ${ownerBlacklisted ? "True" : "False"}`, true)
 		.addField("Guild", guild ? `ID: \`${guild.id}\`\nName: ${guild.name}\nBlacklisted: ${guildBlacklisted ? "True" : "False"}\nWhitelisted: ${guildWhitelisted ? "True" : "False"}` : "DM Number", true)
-		.addField("Created, expiry", `• ${number.createdAt}\n• ${new Date(number.expiry)}`, true)
 		.addField("VIP", vipNumber ? "True" : "False", true)
 		.addField("Blocked", number.blocked && number.blocked.length ? number.blocked.join(", ") : "None", true)
-		.addField(`${guild ? "Guild" : "Owner"} strikes`, strikes.length ? strikes.map(s => `${strikes.indexOf(s) + 1}. \`${s.id}\`: ${s.reason}`).join("\n") : "None");
+		.addField(`${guild ? "Guild" : "Owner"} strikes`, strikes.length ? strikes.map(s => `${strikes.indexOf(s) + 1}. \`${s.id}\`: ${s.reason}`).join("\n") : "None", true)
+		.addField("Created, expiry", `• ${number.createdAt}\n• ${new Date(number.expiry)}`);
 	if (guild && guild.icon) embed.setThumbnail(`https://cdn.discordapp.com/icons/${guild.id}/${guild.icon}.png`);
 	return msg.channel.send({ embed: embed });
 };
