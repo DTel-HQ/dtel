4aab51597e4a11000b95b5ea8a0dbb448adf8910
Ed L <beartechtalks@gmail.com>
2020-07-11 14:14:04 +0100

    Update guildCreate.js

--- Events/guildCreate.js
+++ Events/guildCreate.js
@@ -5,8 +5,8 @@ module.exports = async guild => {
 	let ownerBlacklisted = await guild.owner.user.blacklisted;
 	let name = guild.name.replace(/(\*|`|_|~)/, "\\$1").replace(/discord\.(gg|io|me|li)\/([\w\d])+/g, "**Invite Link Censored**").replace(/@(everyone|here)/g, "@\u200b$1");
 	if (guildBlacklisted || ownerBlacklisted) {
-		client.log(`📑 Left ${guild.id} for being on the blacklist.`);
+		client.log(`📑 Left ${guild.id} as it or its owner is on the blacklist.`);
 		return guild.leave();
 	}
-	client.log(`📥 Joined guild \`${guild.id}\`(${name}). Currently in ${client.guilds.cache.size} servers on cluster ${client.shard.id}`);
+	client.log(`📥 Joined guild \`${guild.id}\` (\`${name}`\). Currently in ${client.guilds.cache.size} servers on cluster ${client.shard.id}.`);
 };
