cd0015bbcaaf38761b6cbf86d4403f20559822f3
Mitchell <contact@mitchells.work>
2020-07-11 12:09:32 +0200

    Fix things
    The entire purpose of this is to not have a suffix, right?
    Also should not assume we were called.

--- Commands/Call/backdoor.js
+++ Commands/Call/backdoor.js
@@ -1,7 +1,6 @@
 module.exports = async(client, msg, suffix, call) => {
 	if (!msg.author.support || !call.pickedUp) return;
-	if (msg.channel.id != config.supportChannel) return msg.channel.send({ embed: { color: config.colors.error, title: "Permission error", description: "You can’t use this command outside of `*611` calls." } });
-	if (!suffix) msg.channel.send({ embed: { color: config.colors.info, title: "Command usage", description: ">backdoor" } });
+	if (msg.channel.id !== config.supportChannel) return msg.channel.send({ embed: { color: config.colors.error, title: "Permission error", description: "You can’t use this command outside of `*611` calls." } });
 
-	require("../Support/backdoor.js")(client, msg, await call.to.number.toString());
+	require("../Support/backdoor.js")(client, msg, call.from.number === (await msg.channel.number) ? call.to.number : call.from.number);
 };
