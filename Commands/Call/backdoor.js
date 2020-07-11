8957ee273930a613b2b2ff6d699148009ccd856a
turret <turret@turret-studios.tk>
2020-07-11 05:15:44 -0500

    allow CS agents to backdoor directly from *611 instead of running backdoor in another channel (#244)
    * add the backdoor command call variant (took ideas from permcheck.js)
    
    * fix grammar
    
    * Fix things
    
    The entire purpose of this is to not have a suffix, right?
    Also should not assume we were called.
    
    * Also fix my things
    
    Co-authored-by: Ed L <beartechtalks@gmail.com>
    Co-authored-by: Mitchell <contact@mitchells.work>

---
+++ Commands/Call/backdoor.js
@@ -0,0 +1,6 @@
+module.exports = async(client, msg, suffix, call) => {
+	if (!msg.author.support || !call.pickedUp) return;
+	if (msg.channel.id !== config.supportChannel) return msg.channel.send({ embed: { color: config.colors.error, title: "Permission error", description: "You can’t use this command outside of `*611` calls." } });
+
+	require("../Support/backdoor.js")(client, msg, call.from.number === (await msg.channel.number).id ? call.to.number : call.from.number);
+};
