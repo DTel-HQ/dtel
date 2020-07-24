dee4b78435c3a2192618eeef363cb63fc6c22582
Ed L <beartechtalks@gmail.com>
2020-07-24 18:16:11 +0100

    feat(callHandler): show attachment sender ID if calling 611

--- Internals/callHandler.js
+++ Internals/callHandler.js
@@ -31,7 +31,7 @@ module.exports = async(cmd, msg, suffix, call) => {
 		let attachment = msg.attachments.first();
 		embed = new MessageEmbed()
 			.setColor(config.colors.info)
-			.setAuthor(hidden ? `Anonymous#${msg.author.id.slice(-4)}` : msg.author.tag)
+			.setAuthor(`Sent by ${hidden ? `Anonymous#${msg.author.id.slice(-4)}` : msg.author.tag}${toSendSupport ? ` (${msg.author.id})` : ""}`)
 			.setTitle("Attachment")
 			.setURL(attachment.url)
 			.setImage(attachment.url)
