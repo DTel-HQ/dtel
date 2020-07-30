8cf467ddc8ce5d3aad16ac5bb6df4d3a87aca7d2
SunburntRock89 <SunburntRock89@gmail.com>
2020-07-30 18:06:52 +0100

    Update guildDelete.js

--- Events/guildDelete.js
+++ Events/guildDelete.js
@@ -2,5 +2,5 @@ const auth = require("../Configuration/auth.js");
 
 module.exports = async guild => {
 	let name = 	guild.name.replace(/(\*|`|_|~)/, "\\$1").replace(/discord\.(gg|io|me|li)\/([\w\d])+/g, "**Invite Link Censored**").replace(/@(everyone|here)/g, "@\u200b$1");
-	client.log(`:outbox_tray: Left guild \`${guild.id}\` (\`${name.replace(/<!?@1(\d){17,19}>/gm, "**Mention Censored**").replace(/@(everyone|here)/g, "@\u200b$1")}\`). Currently in ${client.guilds.cache.size} servers on cluster ${client.shard.id}.`);
+	client.log(`:outbox_tray: Left guild \`${guild.id}\` (\`${name.replace(/<@!?(\d){17,19}>/gm, "**Mention Censored**").replace(/@(everyone|here)/g, "@\u200b$1")}\`). Currently in ${client.guilds.cache.size} servers on cluster ${client.shard.id}.`);
 };
