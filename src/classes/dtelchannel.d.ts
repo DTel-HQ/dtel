17a34d0a45ec0556bc090466af3da080af561c70
turret <turret@turret-studios.tk>
2020-08-05 18:01:31 -0500

    extend TextChannel instead of just the default Channel

--- src/classes/dtelchannel.d.ts
+++ src/classes/dtelchannel.d.ts
@@ -1,6 +1,6 @@
 import * as djs from 'discord.js';
 
-export default class DTelChannel extends djs.Channel {
+export default class DTelChannel extends djs.TextChannel {
     get number(): Array<number>;
 
     get call(): boolean;
