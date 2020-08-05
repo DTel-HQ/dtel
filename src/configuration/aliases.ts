f704761cada677418edd94fa437e66d03efe4dc7
turret <turret@turret-studios.tk>
2020-08-05 17:13:39 -0500

    make aliases a ts file

---
+++ src/configuration/aliases.ts
@@ -0,0 +1,23 @@
+export const aliases = {
+	addcredits: "addcredit",
+	bal: "balance",
+	bc: "broadcast",
+	bl: "blacklist",
+	cmds: "help",
+	commands: "help",
+	contact: "contacts",
+	exchange: "convert",
+	credits: "balance",
+	delstrike: "rmstrike",
+	id: "identify",
+	invite: "links",
+	link: "links",
+	mentions: "mention",
+	msg: "message",
+	pc: "permcheck",
+	perm: "permcheck",
+	promo: "promote",
+	unassign: "deassign",
+	vote: "links",
+	wl: "whitelist"
+}
