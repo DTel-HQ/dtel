5c1b452b9300b48f132fb0ff574b58e66a0740e4
Mitchell3514 <10900096+Mitchell3514@users.noreply.github.com>
2020-08-09 22:37:52 +0200

    wait...

--- Internals/jobs.js
+++ Internals/jobs.js
@@ -367,7 +367,7 @@ scheduleJob("0 0 */12 * * *", async() => {
 scheduleJob("0 */15 * * * *", () => {
 	client.channels.fetch("742110866891210774").then(channel => {
 		channel.messages.fetch({ limit: 1 }).then(message => {
-			if (message.createdTimestamp > Date.now() - (15 * 60 * 60)) channel.bulkDelete();
+			if (message.createdTimestamp < Date.now() - (15 * 60 * 60)) channel.bulkDelete();
 		});
 	});
 });
