6091fd2930436254eaa84f0cdc6e38e1b5e11c36
Mitchell3514 <10900096+Mitchell3514@users.noreply.github.com>
2020-08-09 16:13:23 +0200

    refactor(cinfo): show all users' tags

--- Commands/Support/cinfo.js
+++ Commands/Support/cinfo.js
@@ -4,9 +4,10 @@ module.exports = async(client, msg, suffix) => {
 	if (!call) call = await r.table("Calls").get(suffix);
 	if (!call) return msg.channel.send({ embed: { color: config.colors.error, title: "No call", description: "Couldn't find a call with that ID..." } });
 
-	const [pickedUpUser, hungUpUser] = await Promise.all([
+	const [pickedUpUser, hungUpUser, startedByUser] = await Promise.all([
 		call.pickedUpBy ? client.api.users(call.pickedUpBy).get() : null,
 		call.hungupBy ? client.api.users(call.hungupBy).get() : null,
+		call.startedBy ? client.api.users(call.startedBy).get() : null,
 	]);
 
 	let embed = new MessageEmbed()
@@ -15,6 +16,6 @@ module.exports = async(client, msg, suffix) => {
 		.setDescription(`Showing details for call: \`${call.id}\`\nUse \`>identify\` to identify people from the bot's messages.`)
 		.addField("From", `**Number:** ${call.from.number}\n**Channel:** \`${call.from.channel}\`\n**Hidden:** ${call.from.hidden}\n**Custom name:** ${call.from.name}`, true)
 		.addField("To", `**Number:** ${call.to.number}\n**Channel:** \`${call.to.channel}\`\n**Hidden:** ${call.to.hidden}\n**Custom name:** ${call.to.name}`, true)
-		.addField("General", `**Picked up:** ${call.pickedUp ? call.pickedUpBy ? `${pickedUpUser.username}#${pickedUpUser.discriminator} (${call.pickedUpBy})` : "True" : "False"}\n**Hung up:** ${call.hungupBy ? `${hungUpUser.username}#${hungUpUser.discriminator} (${call.hungupBy})` : "Unknown"}\n**Random call:** ${!!call.rcall}\n**Transferred by:** ${call.transferredBy ? call.transferredBy : "no one"}\n**Started at:** ${call.startedAt}\n**Started by:** ${call.startedBy}`);
+		.addField("General", `**Picked up:** ${call.pickedUp ? call.pickedUpBy ? `${pickedUpUser.username}#${pickedUpUser.discriminator} (${call.pickedUpBy})` : "True" : "False"}\n**Hung up:** ${call.hungupBy ? `${hungUpUser.username}#${hungUpUser.discriminator} (${call.hungupBy})` : "Unknown"}\n**Random call:** ${!!call.rcall}\n**Transferred by:** ${call.transferredBy ? call.transferredBy : "no one"}\n**Started at:** ${call.startedAt}\n**Started by:** ${call.startedBy ? `${startedByUser.username}#${startedByUser.discriminator} (${call.startedBy})` : "Unknown"}`);
 	msg.channel.send({ embed: embed });
 };
