a966f8ab94ad5645ef35fcecd0c03432e98fe337
Ed L <beartechtalks@gmail.com>
2020-08-20 20:38:33 +0100

    strike: better embed

--- Commands/Support/strike.js
+++ Commands/Support/strike.js
@@ -58,7 +58,7 @@ module.exports = async(client, msg, suffix) => {
 		}
 	}
 
-	await msg.channel.send({ embed: { color: config.colors.success, description: `This ${user ? "user" : "guild"} has been striked and now has ${totalStrikes.length} strike(s). StrikeID: \`${id}\`` }, author: { text: msg.author.tag, icon_url: msg.author.displayAvatarURL() } });
+	await msg.channel.send({ embed: { color: config.colors.success, title: "User striked", description: `This ${user ? "user" : "guild"} has been striked and now has ${totalStrikes.length} strike(s).\nStrike ID: \`${id}\`` }, author: { text: msg.author.tag, icon_url: msg.author.displayAvatarURL() } });
 	client.log(`:cloud_lightning: ${channel ? `Guild` : `User`} ID \`${toStrike}\` has been strikken with ID ${strikeDoc.id} by \`${msg.author.tag}\`. (${totalStrikes.length} strikes)`);
 	if (user) {
 		(await user.createDM()).send({ embed: { color: config.colors.info, title: "You received a strike", description: `You have been striked due to the following reason: ${reason}.\n\nYou will get blacklisted after receiving ${3 - totalStrikes.length} more strikes.` } }).catch(e => msg.channel.send({ embed: { color: config.colors.error, title: "Error", description: "Couldn't reach the user. Please manually notify them of their strike." } }));
