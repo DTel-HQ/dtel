cf6931208a0387eb84b71b46ebb0dc4e0ec613b1
Mitchell <contact@mitchells.work>
2020-08-28 13:55:32 +0200

    Update rmstrike.js

--- Commands/Support/rmstrike.js
+++ Commands/Support/rmstrike.js
@@ -1,7 +1,8 @@
 module.exports = async(client, msg, suffix) => {
 	if (!suffix) return msg.channel.send({ embed: { color: config.colors.info, title: "Command usage", description: "Syntax: >rmstrike [strikeID]" } });
 
-	let strikeID = suffix.split(" ")[0];
+	const strikeID = suffix.split(" ")[0];
+	const silent = suffix.split(" ")[1] === "-s";
 	let strike = await r.table("Strikes").get(strikeID);
 
 	if (!strike) return msg.channel.send({ embed: { color: config.colors.error, title: "Invalid ID", description: "How'd you get the ID wrong mate?" } });
@@ -19,5 +20,5 @@ module.exports = async(client, msg, suffix) => {
 
 	let totalStrikes = (await r.table("Strikes").getAll(strike.offender, { index: "offender" }).default([])).length;
 
-	await (await client.users.cache.get(strike.offender).createDM()).send({ embed: { color: config.colors.info, title: "A strike has been removed", description: `A customer support agent removed strike \`${strikeID}\` from your strikes. You now have ${totalStrikes} strike(s) and need ${3 - totalStrikes} more to get blacklisted.` } });
+	if (!silent) (await client.users.cache.get(strike.offender).createDM()).send({ embed: { color: config.colors.info, title: "A strike has been removed", description: `A customer support agent removed strike \`${strikeID}\` from your strikes. You now have ${totalStrikes} strike(s) and need ${3 - totalStrikes} more to get blacklisted.` } });
 };
