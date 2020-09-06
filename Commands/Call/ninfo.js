6134490cd641ff4a8e01a46461896119977a13d4
Rhys O'Kane <SunburntRock89@gmail.com>
2020-09-06 18:34:27 +0100

    Fix it

--- Commands/Call/ninfo.js
+++ Commands/Call/ninfo.js
@@ -3,5 +3,5 @@ module.exports = async(client, msg, suffix, call) => {
 	if (msg.channel.id !== config.supportChannel) return msg.channel.send({ embed: { color: config.colors.error, title: "Permission error", description: "You can’t use this command outside of `*611` calls." } });
 
 	const newSuffix = call.from.number === config.aliasNumbers["*611"] ? call.from.number : call.to.number;
-	require("../Support/ninfo.js")(client, msg, call.id);
+	require("../Support/ninfo.js")(client, msg, newSuffix);
 };
