b7dda51958a232e45dad3be87a5cfa37713f63ee
Mitchell <contact@mitchells.work>
2020-09-13 14:55:49 +0200

    Update contacts.js

--- Commands/Public/contacts.js
+++ Commands/Public/contacts.js
@@ -45,7 +45,7 @@ module.exports = async(client, msg, suffix) => {
 		const testFunction = m => {
 			if (m.author.id !== msg.author.id) return false;
 			if (contacts.length && parseInt(m.content) && parseInt(m.content) <= contacts.length) return true;
-			if (["add", "0"].includes(m.content.toLowerCase())) return true;
+			if (["add", "0", "message"].includes(m.content.toLowerCase())) return true;
 			const index = m.content.split(" ")[1] ? parseInt(m.content.split(" ")[1]) : null;
 			if (!index) return false;
 			const type = m.content.split(" ")[0].toLowerCase();
