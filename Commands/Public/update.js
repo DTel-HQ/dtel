b68e48cfce5f72076c17f9bea6ee9130746f87c3
Mitchell3514 <10900096+Mitchell3514@users.noreply.github.com>
2020-10-27 19:49:14 +0100

    feature(update): allow addition and removal of roles

--- Commands/Public/update.js
+++ Commands/Public/update.js
@@ -1,6 +1,13 @@
+const perms = {
+	manager: config.managerRole,
+	support: config.supportRole,
+	donator: config.donatorRole,
+	contributor: config.contributorRole,
+};
+
 module.exports = async(client, msg, suffix) => {
 	if (suffix === "info") return msg.channel.send({ embed: { color: config.colors.info, description: "update a user their perms\n`>update [userID]`" } });
-	if (!suffix) suffix = msg.author.id;
+	if (!msg.author.support) suffix = msg.author.id;
 	if (msg.mentions.users.first()) suffix = msg.mentions.users.first().id;
 
 	const supportGuild = client.guilds.cache.get(config.supportGuild) || await client.guilds.fetch(config.supportGuild)
@@ -8,6 +15,24 @@ module.exports = async(client, msg, suffix) => {
 	const member = await supportGuild.members.fetch(suffix)
 		.catch(e => msg.channel.send({ embed: { color: config.colors.error, description: "Couldn't fetch member in HQ server" } }));
 
-	const perms = await require("../../Internals/modules").updatePerms(member);
-	msg.channel.send({ embed: { description: `\`\`\`js\n${require("util").inspect(perms)}\`\`\`` } });
+	let resstr = "";
+	for (let perm of suffix.match(/[+-]\w*/g)) {
+		const permName = perm.slice(1);
+		if (!perms[permName]) continue;
+		try {
+			if (perm.startsWith("+")) {
+				member.roles.add(perms[permName], `Addition requested by ${msg.author.id}`);
+				resstr += `✅ ${permName}\n`;
+			}	else {
+				member.roles.remove(perms[permName], `Removal requested by ${msg.author.id}`);
+				resstr += `❌ ${permName}\n`;
+			}
+		} catch (e) {
+			resstr += `Couldn't ${perm.startsWith("+") ? "add" : "remove"} the ${perm} role`;
+		}
+	}
+	if (resstr.length) resstr += "\n";
+
+	const newPerms = await require("../../Internals/modules").updatePerms(member);
+	msg.channel.send({ embed: { description: `resstr\`\`\`js\n${require("util").inspect(newPerms)}\`\`\`` } });
 };
