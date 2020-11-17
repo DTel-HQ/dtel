899d566315ec49eb405b5db0386fa8b8dc5e361a
Mitchell <contact@mitchells.work>
2020-11-17 19:24:48 +0100

    and... done

--- Commands/Public/coinflip.js
+++ Commands/Public/coinflip.js
@@ -1,8 +1,8 @@
 // @ts-check
 module.exports = async(client, msg, suffix) => {
-    if (!suffix || !["head", "tails"].includes(suffix.split(" ")[0].toLowerCase())) return msg.channel.send({ embed: { color: config.colors.info, description: "Are you confident in your guessing skills? Then ~~waste~~ win some money with coinflip!\nUsage: `>coinflip [head|tails] [amount]`" } });
+    if (!suffix || !["heads", "tails"].includes(suffix.split(" ")[0].toLowerCase())) return msg.channel.send({ embed: { color: config.colors.info, description: "Are you confident in your guessing skills? Then ~~waste~~ win some money with coinflip!\nUsage: `>coinflip [heads|tails] [amount]`" } });
 
-    const type = suffix.split(" ")[0].toLowerCase() === "head" ? 0 : 1;
+    const type = suffix.split(" ")[0].toLowerCase() === "heads" ? 0 : 1;
     let bet = parseInt(suffix.split(" ")[1]);
 
     if (!bet) return msg.channel.send({ embed: { color: config.colors.info, description: "We're not playing for free... `>bet [amount]`" } });
@@ -18,13 +18,13 @@ module.exports = async(client, msg, suffix) => {
     return msg.channel.send({ embed: { color, description: "Flipping the coin..." } }).then(async(m) => {
         const balance = (await msg.author.account()).balance + ((side === type ? .5 : -1) * bet)
         await r.table("Accounts").get(msg.author.id).update({ balance });
-        const title = `Landed on...`
+        const title = `Landed on ${side === 0 ? "heads" : "tails"}`
         const description = type === side ? 
             `Congratulations! You've won <:DTS:668551813317787659>${bet * .5}`
             : `Thanks for your credits, I'll be ~~gambling~~ spending them well!`;
 
         setTimeout(() => {
             m.edit({ embed: { title, description, color } });
-        }, 4500);
+        }, 2500);
     }); 
 };
