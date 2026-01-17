94d66bab83120329dd5947cf485d5d0fae700039
Rhys O'Kane <SunburntRock89@gmail.com>
2026-01-17 17:28:22 +0000

    remove unused re2

--- src/dtel.ts
+++ src/dtel.ts
@@ -1,7 +1,10 @@
 import { initInternationalization } from "@src/internationalization/i18n";
 import { populateBlacklistCache } from "./database/db";
 import SharderMessageEvent from "./events/sharderMessage";
-import { prepareClient } from "@src/instances/client";
+import { client, prepareClient } from "@src/instances/client";
+import { EmbedBuilder } from "discord.js";
+import winston from "winston";
+import config from "./config/config";
 
 initInternationalization();
 populateBlacklistCache();
@@ -9,3 +12,10 @@ prepareClient();
 
 process.on("message", msg => SharderMessageEvent(msg as Record<string, unknown>));
 
+
+process.setUncaughtExceptionCaptureCallback((error: Error) => {
+	winston.error(`Uncaught Exception: ${error.message}\n${error.stack}`);
+	client.sendCrossShard({
+		embeds: [new EmbedBuilder().setTitle("Uncaught Exception").setDescription(`\`\`\`${error.message}\n${error.stack}\`\`\``).setColor(0xFF0000)],
+	}, config.supportGuild.channels.badLogs);
+});
